b7c7be3c1e1d2be1e26e137ef0b510cef6e75f2c
Abdalrhman Mohamed <32971963+abdoo8080@users.noreply.github.com>
2022-01-12 18:14:42 -0600

    Use constant exprs instead of constant strings. (#6)
    * Use constant exprs instead of constant strings.
    
    Signed-off-by: Abdalrhman M Mohamed <abdalrhman-mohamed@uiowa.edu>
    
    * Fix bug with negation operator.
    
    Signed-off-by: Abdalrhman M Mohamed <abdalrhman-mohamed@uiowa.edu>
    
    * Remove `Nat.natAbs` for now.
    
    Signed-off-by: Abdalrhman M Mohamed <abdalrhman-mohamed@uiowa.edu>

--- Smt/Constants.lean
+++ Smt/Constants.lean
@@ -10,13 +10,49 @@ open Smt.Term
 open Smt.Solver
 open Lean
 
-def natMinus : Expr := mkConst `Nat.sub
-
 def defNat (s : Solver) : Solver := defineSort s "Nat" [] (Symbol "Int")
 
 def defNatSub (s : Solver) : Solver :=
    defineFun s "Nat.sub" [("x", `"Nat"), ("y", `"Nat")] (`"Nat") $
     `"ite" • (`"<" • `"x" • `"y") • ``"0" • (`"-" • `"x" • `"y")
+
+/-- Map from known constants to corresponding SMT constant/function symbols. -/
+def knownConsts : Std.HashMap Expr (Option Expr) :=
+  List.foldr (fun (k, v) m => m.insert k v) Std.HashMap.empty
+  [
+    (mkConst `Eq [levelOne], mkConst (Name.mkSimple "=")),
+    (mkConst `Ne [levelOne], mkConst `distinct),
+    (mkConst `Prop, mkConst `Bool),
+    (mkConst `True, mkConst `true),
+    (mkConst `False, mkConst `false),
+    (mkConst `Not, mkConst `not),
+    (mkConst `And, mkConst `and),
+    (mkConst `Or, mkConst `or),
+    (mkConst `Iff, mkConst (Name.mkSimple "=")),
+    (mkConst `BEq.beq [levelZero], mkConst (Name.mkSimple "=")),
+    (mkConst `Bool.true, mkConst `true),
+    (mkConst `Bool.false, mkConst `false),
+
+    (mkConst `Neg.neg [levelZero], mkConst (Name.mkSimple "-")),
+    (mkConst `HAdd.hAdd levelsZero, mkConst (Name.mkSimple "+")),
+    (mkConst `HSub.hSub levelsZero, mkConst (Name.mkSimple "-")),
+    (mkConst `HMul.hMul levelsZero, mkConst (Name.mkSimple "*")),
+    (mkConst `HDiv.hDiv levelsZero, mkConst (Name.mkSimple "/")),
+    (mkConst `HMod.hMod levelsZero, mkConst `mod),
+    (mkConst `LT.lt [levelZero], mkConst (Name.mkSimple "<")),
+    (mkConst `GT.gt [levelZero], mkConst (Name.mkSimple ">")),
+    (mkConst `LE.le [levelZero], mkConst (Name.mkSimple "<=")),
+    (mkConst `GE.ge [levelZero], mkConst (Name.mkSimple ">=")),
+
+    (mkApp2 (mkConst `HSub.hSub levelsZero) (mkConst `Nat) (mkConst `Nat),
+     mkConst `Nat.sub),
+    (mkApp2 (mkConst `HDiv.hDiv levelsZero) (mkConst `Nat) (mkConst `Nat),
+     mkConst `div),
+    (mkApp2 (mkConst `HDiv.hDiv levelsZero) (mkConst `Int) (mkConst `Int),
+     mkConst `div)
+  ]
+  where
+    levelsZero := [levelZero, levelZero, levelZero]
 
 end Constants
 end Smt
