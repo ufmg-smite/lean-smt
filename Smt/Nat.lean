392c234bc285ff9f5a016ec6128d23c41ca7e8b7
Abdalrhman Mohamed <32971963+abdoo8080@users.noreply.github.com>
2022-05-04 14:43:10 -0500

    Refactor Transformers infrastructure. (#12)

--- Smt/Nat.lean
+++ Smt/Nat.lean
@@ -7,70 +7,45 @@ open Lean
 open Lean.Expr
 open Smt.Transformer
 
-/-- Traverses `e` and marks type casts of literals to `Nat` for replacement with
-    just the literals. For example, given
-    `(app (app (app (OfNat.ofNat ..) ..) (LIT 0) ..) ..)`, this method should
-    mark the whole expr for replacement with just `(LIT 0)`. -/
-@[Smt] partial def markNatLiterals : Transformer := fun e => do match e with
-  | a@(app f e d)   => match toLiteral f with
-    | none   => markNatLiterals f; markNatLiterals e
-    | some l => addMark a l
-  | lam n t b d     => markNatLiterals t; markNatLiterals b
-  | forallE n t b d => markNatLiterals t; markNatLiterals b
-  | letE n t v b d  => markNatLiterals t; markNatLiterals v; markNatLiterals b
-  | mdata m e s     => markNatLiterals e
-  | proj s i e d    => markNatLiterals e
-  | e               => ()
-  where
-    toLiteral : Expr → Option Expr
-      | app (app (const n ..) ..) l .. => if n == `OfNat.ofNat then l else none
-      | e                              => none
+/-- Removes casts of literals to `Nat` in `e`. For example, given
+    `(app (app (app (OfNat.ofNat ..) ..) (LIT 0) ..) ..)`, this method removes
+    all applications and returns just `(LIT 0)`. -/
+@[Smt] def markNatLiterals : Transformer
+  | app (app (app (const ``OfNat.ofNat ..) ..) l ..) .. => pure l
+  | e                                                   => pure e
 
-/-- Traverses `e` and marks `Nat` constructors `Nat.zero` and `Nat.succ n` for
-    replacement with `0` and `(+ n 1)`. -/
-@[Smt] partial def markNatCons : Transformer := fun e => do match e with
-  | a@(app (const `Nat.succ ..) e d) =>
-    markNatCons e
-    addMark a (plusOne e)
-  | app f e d                => markNatCons f; markNatCons e
-  | lam n t b d              => markNatCons t; markNatCons b
-  | forallE n t b d          => markNatCons t; markNatCons b
-  | letE n t v b d           => markNatCons t; markNatCons v; markNatCons b
-  | mdata m e s              => markNatCons e
-  | proj s i e d             => markNatCons e
-  | e@(const n ..)           => if n == `Nat.zero then
-                                  addMark e (mkLit (Literal.natVal 0))
-  | e                        => ()
+/-- Replaces `Nat` constructors `Nat.zero` and `Nat.succ n` for with `0` and
+    `(+ n 1)`. -/
+@[Smt] def markNatCons : Transformer
+  | const ``Nat.zero ..           => pure $ mkLit (Literal.natVal 0)
+  | app (const ``Nat.succ ..) e _ => do match ← applyTransformations e with
+    | none    => pure $ none
+    | some e' => pure $ plusOne e'
+  | e                             => pure e
   where
     plusOne e :=
       mkApp2 (mkConst (Name.mkSimple "+")) e (mkLit (Literal.natVal 1))
 
-/-- Traverses `e` and marks quantified expressions over natural numbers for
-    replacement with versions that ensure the quantified variables are greater
-    than or equal to 0. For example, given `∀ x : Nat, p(x)`, this method
-    should mark the expr for replacement with `∀ x : Nat, x ≥ 0 → p(x)`. -/
-@[Smt] partial def markNatForalls : Transformer := fun e => markImps' #[] e
+/-- Replaces quantified expressions over natural numbers for with versions that
+    ensure the quantified variables are greater than or equal to 0. For
+    example, given `∀ x : Nat, p(x)`, this method returns the expr
+    `∀ x : Nat, x ≥ 0 → p(x)`. -/
+@[Smt] def markNatForalls : Transformer
+  | e@(forallE n t@(const ``Nat ..) b d) => do
+    -- TODO: replace check with final domain and improve returned result.
+    if ¬e.isArrow then
+      match ← applyTransformations t,
+            ← Meta.withLocalDecl n d.binderInfo t (applyTransformations' b) with
+      | some t', some b' => pure $ mkForall n d.binderInfo t' (imp b')
+      | _      , _       => pure e
+    else return e
+  | e                                   => pure e
   where
-    markImps' xs e := do match e with
-      | app f e d           => markImps' xs f; markImps' xs e
-      | lam n t b d         =>
-        markImps' xs t
-        Meta.withLocalDecl n d.binderInfo t (fun x => markImps' (xs.push x) b)
-      | e@(forallE n t@(const `Nat ..) b d) =>
-        markImps' xs t
-        if ¬(e.instantiate xs).isArrow then
-          addMark e (mkForall n d.binderInfo t (imp b))
-        Meta.withLocalDecl n d.binderInfo t (fun x => markImps' (xs.push x) b)
-      | e@(forallE n t b d) =>
-        markImps' xs t
-        Meta.withLocalDecl n d.binderInfo t (fun x => markImps' (xs.push x) b)
-      | letE n t v b d      =>
-        markImps' xs t
-        markImps' xs v
-        Meta.withLetDecl n t v (fun x => markImps' (xs.push x) b)
-      | mdata m e s         => markImps' xs e
-      | proj s i e d        => markImps' xs e
-      | e                   => ()
+    applyTransformations' b x := do
+      let mut b' ← applyTransformations (b.instantiate #[x])
+      if let some b'' := b' then
+        b' := some (b''.abstract #[x])
+      return b'
     imp e := mkApp2 (mkConst (Name.mkSimple "=>"))
                     (mkApp2 (mkConst (Name.mkSimple ">="))
                             (mkBVar 0)
--- Smt/String.lean
+++ Smt/String.lean
@@ -7,30 +7,20 @@ open Lean
 open Lean.Expr
 open Smt.Transformer
 
-@[Smt] def markStringGetOp : Transformer := fun e => do match e with
-  | a@(app (app (const `String.getOp ..) f _) e _)  =>
-    addMark a (mkApp (mkConst `str.to_code) (mkApp2 (mkConst `str.at) f e))
-    markStringGetOp f
-    markStringGetOp e
-  | app f e _       => markStringGetOp f; markStringGetOp e
-  | lam _ _ b _     => markStringGetOp b
-  | mdata _ e _     => markStringGetOp e
-  | proj _ _ e _    => markStringGetOp e
-  | letE _ _ v b _  => markStringGetOp v; markStringGetOp b
-  | forallE _ t b _ => markStringGetOp t; markStringGetOp b
-  | _               => ()
+@[Smt] def transformStringGetOp : Transformer
+  | app (app (const `String.getOp ..) f _) e _ => do
+    return match ← applyTransformations f, ← applyTransformations e with
+    | some f', some e' =>
+      mkApp (mkConst `str.to_code) (mkApp2 (mkConst `str.at) f' e')
+    | _      , _       => none
+  | e                                          => return e
 
-@[Smt] def markStringContains : Transformer := fun e => do match e with
-  | a@(app (app (const `String.contains ..) f _) e _)  =>
-    addMark a (mkApp2 (mkConst `str.contains) f (mkApp (mkConst `str.from_code) e))
-    markStringGetOp f
-    markStringGetOp e
-  | app f e _       => markStringContains f; markStringContains e
-  | lam _ _ b _     => markStringContains b
-  | mdata _ e _     => markStringContains e
-  | proj _ _ e _    => markStringContains e
-  | letE _ _ v b _  => markStringContains v; markStringContains b
-  | forallE _ t b _ => markStringContains t; markStringContains b
-  | _               => ()
+@[Smt] def transformStringContains : Transformer
+  | app (app (const `String.contains ..) f _) e _ => do
+    return match ← applyTransformations f, ← applyTransformations e with
+    | some f', some e' =>
+      mkApp2 (mkConst `str.contains) f' (mkApp (mkConst `str.from_code) e')
+    | _      , _       => none
+  | e                                             => return e
 
 end Smt.String
