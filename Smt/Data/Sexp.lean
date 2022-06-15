e1ec4b1127da70838d373dbaa39573d940aaa4c2
Abdalrhman Mohamed <32971963+abdoo8080@users.noreply.github.com>
2022-06-15 11:43:45 +0300

    Add Apache 2.0 license. (#18)
    * Add licensing information.
    
    Signed-off-by: Abdalrhman M Mohamed <abdalrhman-mohamed@uiowa.edu>
    
    * Add a license comment on top of source files.
    
    Signed-off-by: Abdalrhman M Mohamed <abdalrhman-mohamed@uiowa.edu>
    
    * Add a list of contributers.
    
    Signed-off-by: Abdalrhman M Mohamed <abdalrhman-mohamed@uiowa.edu>
    
    * Reword.
    
    Signed-off-by: Abdalrhman M Mohamed <abdalrhman-mohamed@uiowa.edu>

--- Smt/Data/Sexp.lean
+++ Smt/Data/Sexp.lean
@@ -1,3 +1,10 @@
+/-
+Copyright (c) 2021-2022 by the authors listed in the file AUTHORS and their
+institutional affiliations. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Wojciech Nawrocki
+-/
+
 /-- The type of S-expressions. -/
 inductive Sexp where
   | atom : String → Sexp
