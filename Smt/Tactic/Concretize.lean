4ea5d3f4a0c5fcd0950677ee7d0bef3afad7afc4
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2022-06-21 18:23:17 -0400

    feat: improve concretization order

--- Smt/Tactic/Concretize.lean
+++ Smt/Tactic/Concretize.lean
@@ -20,12 +20,13 @@ def rewriteTarget (e : Expr) (symm : Bool) (config : Rewrite.Config) : TacticM U
     let mvarId' ← replaceTargetEq (← getMainGoal) r.eNew r.eqProof
     replaceMainGoal (mvarId' :: r.mvarIds)
 
-def rewriteLocalDecl (e : Expr) (symm : Bool) (fvarId : FVarId) (config : Rewrite.Config) : TacticM Unit := do
+def rewriteLocalDecl (e : Expr) (symm : Bool) (fvarId : FVarId) (config : Rewrite.Config) : TacticM FVarId := do
   Term.withSynthesize <| withMainContext do
     let localDecl ← getLocalDecl fvarId
     let rwResult ← rewrite (← getMainGoal) localDecl.type e symm (config := config)
     let replaceResult ← replaceLocalDecl (← getMainGoal) fvarId rwResult.eNew rwResult.eqProof
     replaceMainGoal (replaceResult.mvarId :: rwResult.mvarIds)
+    return replaceResult.fvarId
 
 /-- Try to rewrite using `pf : _ = _` everywhere except for local declarations matching `skipPred`. -/
 def tryRewriteEverywhere (pf : Expr) (skipPred : LocalDecl → Bool) : TacticM Unit := do
@@ -35,7 +36,7 @@ def tryRewriteEverywhere (pf : Expr) (skipPred : LocalDecl → Bool) : TacticM U
     let lctx ← getLCtx
     for localDecl in lctx do
       if skipPred localDecl then continue
-      try Meta.rewriteLocalDecl pf false localDecl.fvarId {}
+      try let _ ← Meta.rewriteLocalDecl pf false localDecl.fvarId {}
       catch _ => pure ()
 
 end Lean.Meta
@@ -47,41 +48,54 @@ open Lean Elab Meta Tactic
 /-- Some crap we need to store while processing the tactic. -/
 structure ConcretizationData where
   eConcrete : Expr
-  /-- `var.$nm : _` created by `generalize` -/
+  /-- `var.$nm : _` created by `generalize` 
+  These should not be rewritten, so we keep the `FVarId`. -/
   fvVar : FVarId
-  /-- `eqVar.$nm : $eConcrete = var.$nm` created by `generalize` -/
+  /-- `eqVar.$nm : $eConcrete = var.$nm` created by `generalize`
+  These should not be rewritten, so we keep the `FVarId`. -/
   fvEqVar : FVarId
   /-- `eqBody.$nm : var.$nm = $eBody`
 
   We use this equality to simulate a let-binding since it is not easy to rewrite
   in a let-binding body. Note that other concretization steps may recursively rewrite
-  in `eBody`. -/
-  fvEqBody : FVarId
+  in `eBody`, so we only remember the name as the `FVarId` might change. -/
+  nmEqBody : Name
 
-def ConcretizationData.create (nm : Name) (eConcrete : Expr) : TacticM ConcretizationData := 
-  liftMetaTacticAux fun mvarId => do
-    let varName := `var ++ nm
-    let eqVarName := `eqVar ++ nm
-    let eqBodyName := `eqBody ++ nm
+def ConcretizationData.create (nm : Name) (eConcrete : Expr) : TacticM ConcretizationData := do
+  let conc ← liftMetaTacticAux fun mvarId => do
+    let varName := nm.modifyBase (· ++ `var)
+    let eqVarName := nm.modifyBase (· ++ `eqVar)
+    let eqBodyName := nm.modifyBase (· ++ `eqBody)
 
     let (#[fvVar, fvEqVar], mvarId) ← Meta.generalize mvarId #[{
         expr := eConcrete
         xName? := varName
         hName? := eqVarName
+
       }] | throwError "unexpected generalize fvars"
 
+    let ret := { eConcrete, fvVar, fvEqVar, nmEqBody := eqBodyName }
+    return (ret, [mvarId])
+
+  liftMetaTactic fun mvarId => do
     -- `@f a b` ≡ `(fun x y z w => t) a b` ≡ `fun z w => t[a/x,b/y]`
     let eBody ← withTransparency TransparencyMode.all <| whnf eConcrete
-    let tpEqBody ← mkEq eConcrete eBody
-    let pfEqBody ← mkEqRefl eConcrete
-    let (fvEqBody, mvarId) ← intro1P (← assert mvarId eqBodyName tpEqBody pfEqBody)
+    let tpEqBody ← mkEq (mkFVar conc.fvVar) eBody
+    let pfEqBody ← mkEqSymm (mkFVar conc.fvEqVar)
+    let (_, mvarId) ← intro1P (← assert mvarId conc.nmEqBody tpEqBody pfEqBody)
 
-    let ret := { eConcrete, fvVar, fvEqVar, fvEqBody }
-    return (ret, [mvarId])
+    return [mvarId]
+  
+  return conc
+
+def ConcretizationData.getDeclEqBody (conc : ConcretizationData) : TacticM LocalDecl :=
+  withMainContext do
+    getLocalDeclFromUserName conc.nmEqBody
 
 def ConcretizationData.getBody (conc : ConcretizationData) : TacticM Expr :=
   withMainContext do
-    let tp ← inferType (mkFVar conc.fvEqBody)
+    let localDecl ← conc.getDeclEqBody
+    let tp ← inferType (mkFVar localDecl.fvarId)
     let some (_, _, eBody) := tp.eq?
       | throwError "internal error, expected equality but got {tp}"
     return eBody
@@ -104,9 +118,9 @@ abbrev ConcretizeM := StateT State TacticM
 def withMainContext (x : ConcretizeM α) : ConcretizeM α := do
   controlAt TacticM fun mapInBase => Tactic.withMainContext (mapInBase x)
 
-def traceLCtx : MetaM Unit := traceCtx `smt.debug.lctx do
-  for localDecl in (← getLCtx) do
-    trace[smt.debug.concretize.lctx] "{localDecl.userName} : {localDecl.type} {if localDecl.isAuxDecl then "(aux)" else ""})"
+def traceLCtx : TacticM Unit := traceCtx `smt.debug.lctx <| Tactic.withMainContext do
+  for ld in (← getLCtx) do
+    trace[smt.debug.concretize.lctx] "{ld.userName} @ {repr ld.fvarId} : {ld.type} {if ld.isAuxDecl then "(aux)" else ""}"
 
 def getCached? (e : Expr) : ConcretizeM (Option Name) := do
   -- We use `withExtra` as concretizations can be prefixes of the full application.
@@ -166,7 +180,7 @@ def concretizeApp (e : Expr) : ConcretizeM TransformStep := do
         let txt := txt.replace " " "_"
         return nm ++ Name.mkSimple txt
       else return nm
-    let nm ← mkFreshUserName nm
+    -- let nm ← mkFreshUserName nm
 
     addNewConcretization nm eConcrete
     foundConcretizable nm
@@ -191,7 +205,12 @@ partial def loop : ConcretizeM Unit := traceCtx `smt.debug.concretize.loop do
   let fcs := (← get).foundConcretizables
   for nm in fcs do
     let some conc := (← get).concretizations.find? nm | throwError "internal error, missing concretization '{nm}'"
-    tryRewriteEverywhere (mkFVar conc.fvEqVar) (fun ld => ld.isAuxDecl || ld.fvarId == conc.fvEqVar || ld.fvarId == conc.fvEqBody)
+    let declEqBody ← conc.getDeclEqBody
+    tryRewriteEverywhere (mkFVar conc.fvEqVar) fun ld =>
+      ld.isAuxDecl ||
+      (`var).isSuffixOf ld.userName.eraseMacroScopes ||
+      (`eqVar).isSuffixOf ld.userName.eraseMacroScopes ||
+      ld.fvarId == declEqBody.fvarId
 
   if !fcs.isEmpty then
     loop
@@ -211,21 +230,35 @@ partial def loop : ConcretizeM Unit := traceCtx `smt.debug.concretize.loop do
 def evalConcretize : ConcretizeM Unit := do
   loop
 
-  -- At the end, clear the hypotheses by replacing them with let-bindings.
-  for (nm, conc) in (← get).concretizations do
-    let eBody ← conc.getBody
-    let fvLet ← liftMetaTacticAux fun mvarId => do
-      -- `$concNm := $eBody`
-      let (fv, mvarId) ← intro1P (← define mvarId nm (← inferType eBody) eBody)
-      return (fv, [mvarId])
+  traceLCtx
+  withMainContext do
+    let mut fvars := #[]
+    for ld in ← withMainContext getLCtx do
+      if (`var).isSuffixOf ld.userName.eraseMacroScopes then
+        fvars := fvars.push ld.fvarId
+    fvars ← sortFVarIds fvars
 
-    withMainContext do
-      -- `var.$concNm = $concNm`
-      let eqTp ← mkEq (mkFVar conc.fvVar) (mkFVar fvLet)
-      let eq ← mkExpectedTypeHint (mkFVar conc.fvEqBody) eqTp
-      tryRewriteEverywhere eq (fun _ => true)
+    for fv in fvars.reverse do
+      let nm := (← getLocalDecl fv).userName.eraseMacroScopes.getPrefix
+      let some conc := (← get).concretizations.find? nm | throwError "internal error, missing concretization '{nm}'"
+      let eBody ← conc.getBody
+      let fvLet ← liftMetaTacticAux fun mvarId => do
+        -- `$concNm := $eBody`
+        let (fv, mvarId) ← intro1P (← define mvarId nm (← inferType eBody) eBody)
+          return (fv, [mvarId])
 
-    evalTactic <| ← `(tactic| clear $(mkIdent (`eqBody ++ nm)) $(mkIdent (`eqVar ++ nm)) $(mkIdent (`var ++ nm)))
+      withMainContext do
+        -- `var.$concNm = $concNm`
+        let eqTp ← mkEq (mkFVar conc.fvVar) (mkFVar fvLet)
+        let declEqBody ← conc.getDeclEqBody
+        let eq ← mkExpectedTypeHint declEqBody.toExpr eqTp
+        tryRewriteEverywhere eq fun ld =>
+          ld.isAuxDecl ||
+          (`var).isSuffixOf ld.userName.eraseMacroScopes ||
+          (`eqVar).isSuffixOf ld.userName.eraseMacroScopes ||
+          ld.fvarId == declEqBody.fvarId
+
+      evalTactic <| ← `(tactic| clear $(mkIdent (nm ++ `eqBody)) $(mkIdent (nm ++ `eqVar)) $(mkIdent (nm ++ `var)))
 
 /-- Recursively finds and replaces concretizations in the goal and the hypotheses.
 
@@ -273,3 +306,19 @@ set_option trace.smt.debug true in
 example : ([1,2,3] : List Int).foldl Int.add 0 = 6 := by
   concretize
   rfl
+
+def BitVec (w : Nat) := Fin (2^w)
+protected def BitVec.zero (w : Nat) : BitVec w :=
+  ⟨0, Nat.pos_pow_of_pos _ <| by decide⟩
+instance : Inhabited (BitVec w) := ⟨BitVec.zero w⟩
+opaque BitVec.xor {w : Nat} : BitVec w → BitVec w → BitVec w
+
+def polyAdd {w : Nat} : BitVec w → BitVec w → BitVec w :=
+  BitVec.xor
+def polyDouble {w : Nat} (x : BitVec w) : BitVec w :=
+  polyAdd x x
+
+set_option trace.smt.debug true in
+example (x y : BitVec 2) : polyDouble (polyAdd x y) = polyDouble (polyAdd y x) := by
+  concretize
+  sorry
