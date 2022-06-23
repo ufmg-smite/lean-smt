7fd5ed8ef30216d3b14f97c8e67dcf01e539c588
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2022-06-23 12:17:01 -0400

    feat: add concretization tests

--- Smt/Tactic/Concretize.lean
+++ Smt/Tactic/Concretize.lean
@@ -355,36 +355,3 @@ elab "concretize" "[" nms:ident,* "]" : tactic => do
     |>.run { concretizeSet }
 
 end Smt.Concretize
-
-namespace test
-
-def generalAdd [Add α] (a b : α) := a + b
-
-set_option trace.smt.debug true in
-example : @generalAdd Int _ 3 3 = (6 : Int) := by
-  concretize [generalAdd]
-  sorry
-
-def BitVec (w : Nat) := Fin (2^w)
-protected def BitVec.zero (w : Nat) : BitVec w :=
-  ⟨0, Nat.pos_pow_of_pos _ <| by decide⟩
-instance : Inhabited (BitVec w) := ⟨BitVec.zero w⟩
-opaque BitVec.xor {w : Nat} : BitVec w → BitVec w → BitVec w
-
-def polyAdd {w : Nat} : BitVec w → BitVec w → BitVec w :=
-  BitVec.xor
-def polyDouble {w : Nat} (x : BitVec w) : BitVec w :=
-  polyAdd x x
-
-set_option trace.smt.debug true in
-example (x y : BitVec 2) : polyDouble (polyAdd x y) = polyDouble (polyAdd y x) := by
-  concretize [polyDouble, polyAdd]
-  sorry
-
-set_option trace.smt.debug true in
-example : ([1,2,3] : List Int).foldl Int.add 0 = 6 := by
-  -- TODO(WN): everything breaks when `whnf` makes no progress on the concretization body
-  -- concretize [List.foldl]
-  rfl
-
-end test
