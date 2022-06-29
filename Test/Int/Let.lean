b0d81f99698b9f69143d2ddf708a2c28736402b6
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2022-06-29 15:35:44 -0400

    feat: we now support let-bindings

--- Test/Int/Let.lean
+++ Test/Int/Let.lean
@@ -10,9 +10,9 @@ example (h : let y := 10; f y = 10) : f 10 = 10 := by
   smt [h]
   exact h
 
--- example (h : f 10 = 10) : f 10 = 10 := by
---   let z : Int := 10
---   have : 10 = z := rfl
---   rw [this]
---   smt [h, z]
---   exact h
+example (h : f 10 = 10) : f 10 = 10 := by
+  let z : Int := 10
+  have : 10 = z := rfl
+  rw [this]
+  smt [h, z]
+  exact h
