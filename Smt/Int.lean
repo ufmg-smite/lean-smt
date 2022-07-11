177bf89f86a837234958ef4612495057ed804910
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2022-07-10 20:11:02 -0400

    feat: add more Int operations

--- Smt/Int.lean
+++ Smt/Int.lean
@@ -15,9 +15,14 @@ open Translator Term
 
 @[smtTranslator] def replaceConst : Translator
   | app (const `Int.ofNat ..) e .. => applyTranslators! e
-  | const `Int.neg ..              => return symbolT "-"
-  | const `Int.le ..               => return symbolT "<="
   | const `Int.add ..              => return symbolT "+"
+  | const `Int.sub ..              => return symbolT "-"
+  | const `Int.neg ..              => return symbolT "-"
+  | const `Int.mul ..              => return symbolT "*"
+  | const `Int.div ..              => return symbolT "div"
+  | const `Int.mod ..              => return symbolT "mod"
+  | const `Int.le ..               => return symbolT "<="
+  | const `Int.lt ..               => return symbolT "<"
   | _                              => return none
 
 end Smt.Int
