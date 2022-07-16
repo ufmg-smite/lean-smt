cbe1443c6592fa8bf3b1ac8f0231115807acb87e
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2022-07-16 13:31:02 -0400

    feat: don't rely on constant branch elimination

--- Test/BitVec/GF256Pow.lean
+++ Test/BitVec/GF256Pow.lean
@@ -1,34 +1,39 @@
+import Lean.Meta.Basic
+import Lean.Meta.FunInfo
+import Lean.Util.MonadCache
+
 import Smt
 import Smt.Data.BitVec
+import Smt.Tactic.EqnDef
+import Std.Data.HashSet
 
 def polyAdd (a b : BitVec w) : BitVec w := a ^^^ b
 
 def polyMul (a : BitVec w) (b : BitVec v) : BitVec (w+v) :=
   let wOut := w + v
-  let a := a.zeroShrinxtend wOut
+  let_opaque a := a.zeroExtend wOut (Nat.le_add_right _ _)
   -- fold over the bits of b starting at MSB
   let ret : BitVec wOut := List.range v |>.foldr (init := 0) fun i acc =>
-    let acc' := acc <<< BitVec.ofNat wOut 1
+    let_opaque acc' := acc <<< BitVec.ofNat wOut 1
     if b.lsbGet i then polyAdd acc' a else acc'
   ret
 
 def polyMod (x : BitVec w) (y : BitVec (v+1)) : BitVec v :=
   if y = 0 then 0
   else
-    let reduce (a : BitVec (v+1)) : BitVec (v+1) := if a.lsbGet (v-1) then polyAdd a y else a
-    let ret : BitVec v := 0
-    let pow : BitVec (v+1) := reduce ⟨1, by
-      show 1 * 2 ≤ 2^v * 2
-      exact Nat.mul_le_mul_right 2 <| Nat.succ_le_of_lt <| Nat.pos_pow_of_pos _ <| by decide⟩
+    let reduce (a : BitVec (v+1)) : BitVec (v+1) :=
+      if a.lsbGet (v-1) then polyAdd a y else a
+    let_opaque ret : BitVec v := 0
+    let_opaque pow : BitVec (v+1) := reduce (BitVec.ofNat (v+1) 1)
     let (ret, _) := List.range w |>.foldl (init := (ret, pow)) fun (ret, pow) i =>
-      let pow := reduce (pow <<< BitVec.ofNat (v+1) 1)
-      if x.lsbGet i then (polyAdd ret (pow.zeroShrinxtend v), pow)
-      else (ret, pow)
+      let_opaque pow := reduce (pow <<< BitVec.ofNat (v+1) 1)
+      let_opaque ret := if x.lsbGet i then polyAdd ret (pow.shrink v) else ret
+      (ret, pow)
     ret
 
 namespace GF256
 
-/-- A field element. -/
+/-- A field element. NB: it does not have to be reduced. -/
 abbrev elt := BitVec 8
 
 def irreducible : BitVec 9 := BitVec.ofNat 9 0b100011011
@@ -49,12 +54,51 @@ where
 def inverse (x : elt) : elt :=
   pow 254 x
 
+/- false ?! -/
+def reduce (x : BitVec w) : elt := polyMod x irreducible
+#eval pow 256 (BitVec.ofNat 8 0xab) == reduce (BitVec.ofNat 8 0xab)
+#eval pow 1 (BitVec.ofNat 8 0xab)
+#eval mul (BitVec.ofNat 8 1) (BitVec.ofNat 8 0xab)
+#eval reduce (BitVec.ofNat 8 0xab)
+#eval pow 1 (BitVec.ofNat 8 0xab) == reduce (BitVec.ofNat 8 0xab)
+
 set_option trace.smt.debug true in
-example (x : elt) : pow 256 x = x := by
-  concretize [pow]
-  let z := pow 256 x
-  have : pow 256 x = z := rfl
-  rw [this]
-  smt [pow, elt]
+-- set_option maxHeartbeats 2000000 in
+-- set_option macRecDepth 2048 in
+set_option trace.Smt.reduce true in
+example (x : elt) : pow 256 x = polyMod x irreducible := by
+  extract_def pow
+  extract_def mul
+  extract_def polyMod
+  specialize_def polyMod.def [16, 8]
+  specialize_def polyMod.def [8, 8]
+  save
+
+  extract_def polyMul
+  specialize_def polyMul.def [8, 8]
+  save
+
+  simp (config := {zeta := false}) only
+    [ polyMul.«8».«8».specialization
+    , polyMod.«16».«8».specialization
+    ]
+    at GF256.mul.def
+  save
+
+  simp (config := {zeta := false}) only
+    [ polyMod.«8».«8».specialization ]
+  save
+
+  -- smt [
+  --   GF256.elt,
+  --   GF256.pow.def,
+  --   GF256.pow.sq,
+  --   GF256.mul.def,
+  --   polyMod.«16».«8».def,
+  --   polyMod.«8».«8».def,
+  --   polyMul.«8».«8».def,
+  --   GF256.irreducible
+  --  ]
+  sorry
 
 end GF256
