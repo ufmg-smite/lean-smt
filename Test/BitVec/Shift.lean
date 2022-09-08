d8cd209d1e8fc8bf6a7c9b0927d2d2feaa2d099c
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2022-09-07 20:23:48 -0400

    fix: test

--- Test/BitVec/Shift.lean
+++ Test/BitVec/Shift.lean
@@ -2,13 +2,11 @@ import Smt
 import Smt.Data.BitVec
 
 theorem append_eq_shl_or_2 (x y : BitVec 2)
-    : x ++ y = (x.zeroExtend 4 (by decide) <<< 2) ||| y.zeroExtend 4 (by decide) := by
-  simp only [BitVec.zeroExtend]
+    : x ++ y = (x.zeroExtend 2 <<< 2) ||| y.zeroExtend 2 := by
   smt
   sorry
 
 theorem append_eq_shl_or_3 (x y : BitVec 3)
-    : x ++ y = (x.zeroExtend 6 (by decide) <<< 3) ||| y.zeroExtend 6 (by decide) := by
-  simp only [BitVec.zeroExtend]
+    : x ++ y = (x.zeroExtend 3 <<< 3) ||| y.zeroExtend 3 := by
   smt
   sorry
