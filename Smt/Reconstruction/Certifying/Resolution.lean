cfeddacb0068e2ffb5d5e4a4ef93304895233dd5
Tomaz Gomes <tomgm1502@gmail.com>
2022-10-24 13:13:07 -0300

    fix order of output or-chain in R2

--- Smt/Reconstruction/Certifying/Resolution.lean
+++ Smt/Reconstruction/Certifying/Resolution.lean
@@ -12,6 +12,14 @@ theorem resolution_thm : ∀ {A B C : Prop}, (A ∨ B) → (¬ A ∨ C) → B �
               | inr cp  => exact (Or.inr cp)
   | inr bp => exact (Or.inl bp)
 
+theorem flipped_resolution_thm : ∀ {A B C : Prop}, (¬ A ∨ B) → (A ∨ C) → B ∨ C := by
+  intros A B C h₁ h₂
+  cases h₁ with
+  | inl nap => cases h₂ with
+               | inl ap => exact False.elim (nap ap)
+               | inr cp => exact (Or.inr cp)
+  | inr bp => exact (Or.inl bp)
+
 theorem resolution_thm₂ : ∀ {A C: Prop}, A → (¬ A ∨ C) → C := λ a ornac =>
   match ornac with
   | Or.inl na => False.elim (na a)
@@ -24,23 +32,27 @@ theorem resolution_thm₃ : ∀ {A B: Prop}, (A ∨ B) → ¬ A → B := λ orab
 
 theorem resolution_thm₄ : ∀ {A : Prop}, A → ¬ A → False := λ a na => na a
 
-def resolutionCore (firstHyp secondHyp : Ident) (pivotTerm : Term) : TacticM Unit := do
-  let fident1 ← mkIdent <$> mkFreshId
-  let fident2 ← mkIdent <$> mkFreshId
+def resolutionCore (firstHyp secondHyp : Ident) (pivotTerm : Term) (flipped : Bool) : TacticM Unit := do
   let notPivot : Term := Syntax.mkApp (mkIdent `Not) #[pivotTerm]
-  let pivotExpr     ← elabTerm pivotTerm none
-  let notPivotExpr  ← elabTerm notPivot none
+  let mut resolvantOne     ← elabTerm pivotTerm none
+  let mut resolvantTwo  ← elabTerm notPivot none
   let firstHypType  ← inferType (← elabTerm firstHyp none)
   let secondHypType ← inferType (← elabTerm secondHyp none)
 
+  if flipped then
+    let tmp      := resolvantOne
+    resolvantOne    := resolvantTwo
+    resolvantTwo := tmp
+
+  let fident1 ← mkIdent <$> mkFreshId
+  let fident2 ← mkIdent <$> mkFreshId
   let lenGoal ← getLength <$> getMainTarget
-  pullCore pivotExpr    firstHypType  firstHyp  fident1
-  pullCore notPivotExpr secondHypType secondHyp fident2
+  pullCore resolvantOne firstHypType  firstHyp  fident1
+  pullCore resolvantTwo secondHypType secondHyp fident2
 
   let mut len₁ := getLength firstHypType
-  if Option.isNone (getIndex pivotExpr firstHypType) then
-    len₁ := len₁ - (getLength pivotExpr) + 1
-
+  if Option.isNone (getIndex resolvantOne firstHypType) then
+    len₁ := len₁ - (getLength resolvantOne) + 1
   let len₂ := getLength secondHypType
 
   if lenGoal > 2 then
@@ -49,20 +61,18 @@ def resolutionCore (firstHyp secondHyp : Ident) (pivotTerm : Term) : TacticM Uni
       logInfo m!"....apply {s}"
       printGoal
 
-  if len₁ > 1 then
-    if len₂ > 1 then
-      evalTactic (← `(tactic| exact resolution_thm $fident1 $fident2))
-      logInfo m!"..close goal with resolution_thm"
-    else
-      evalTactic (← `(tactic| exact resolution_thm₃ $fident1 $fident2))
-      logInfo m!"..close goal with resolution_thm₃"
-  else
-    if len₂ > 1 then
-      evalTactic (← `(tactic| exact resolution_thm₂ $fident1 $fident2))
-      logInfo m!"..close goal with resolution_thm₂"
-    else
-      evalTactic (← `(tactic| exact resolution_thm₄ $fident1 $fident2))
-      logInfo m!"..close goal with resolution_thm₄"
+  let thmName : Name := 
+    match Nat.blt 1 len₁, Nat.blt 1 len₂ with
+    | true, true   => if flipped then `flipped_resolution_thm else `resolution_thm
+    | true, false  => if flipped then `flipped_resolution_thm₂ else `resolution_thm₂
+    | false, true  => if flipped then `flipped_resolution_thm₃ else `resolution_thm₃
+    | false, false => if flipped then `flipped_resolution_thm₄ else `resolution_thm₄
+
+  let thm := mkIdent thmName
+
+  logInfo m!"....closing goal with: {thm}"
+  let proof := ⟨Syntax.mkApp ⟨thm⟩ #[fident1, fident2]⟩
+  evalTactic (← `(tactic| exact $proof))
 
 syntax (name := resolution_1) "R1" ident "," ident "," term : tactic
 @[tactic resolution_1] def evalResolution_1 : Tactic :=
@@ -70,7 +80,7 @@ syntax (name := resolution_1) "R1" ident "," ident "," term : tactic
     let firstHyp : Ident := ⟨stx[1]⟩
     let secondHyp : Ident := ⟨stx[3]⟩
     let pivotTerm : Term := ⟨stx[5]⟩
-    resolutionCore firstHyp secondHyp pivotTerm
+    resolutionCore firstHyp secondHyp pivotTerm false
 
 syntax (name := resolution_2) "R2" ident "," ident "," term : tactic
 @[tactic resolution_2] def evalResolution_2 : Tactic :=
@@ -78,9 +88,12 @@ syntax (name := resolution_2) "R2" ident "," ident "," term : tactic
     let firstHyp : Ident := ⟨stx[1]⟩
     let secondHyp : Ident := ⟨stx[3]⟩
     let pivotTerm : Term := ⟨stx[5]⟩
-    resolutionCore secondHyp firstHyp pivotTerm
+    resolutionCore firstHyp secondHyp pivotTerm true
 
-example : A ∨ B ∨ D ∨ (W ∨ Z) → E ∨ F ∨ G ∨ ¬ (W ∨ Z) → A ∨ B ∨ D ∨ E ∨ F ∨ G := by
+example : A ∨ B ∨ C ∨ D →  E ∨ F ∨ ¬ B ∨ G → E ∨ F ∨ G ∨ A ∨ C ∨ D := by
   intros h₁ h₂
-  R1 h₁, h₂, (W ∨ Z)
+  R2 h₂, h₁, B
 
+example : ¬ (A ∧ B) ∨ C ∨ ¬ D ∨ ¬ A → A ∨ ¬ (A ∧ B) → ¬ (A ∧ B) ∨ C ∨ ¬ D ∨ ¬ (A ∧ B) := by
+  intros h₁ h₂
+  R2 h₁, h₂, A
