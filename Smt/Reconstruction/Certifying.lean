ec2b946ea8ce98fc92b9783f58de50a17419c829
Tomaz Gomes <tomgm1502@gmail.com>
2022-11-14 16:30:54 -0300

    adding liftOrNToNeg to Certifying.lean

--- Smt/Reconstruction/Certifying.lean
+++ Smt/Reconstruction/Certifying.lean
@@ -1,8 +1,10 @@
 import Smt.Reconstruction.Certifying.Boolean
 import Smt.Reconstruction.Certifying.Factor
 import Smt.Reconstruction.Certifying.LiftOrNToImp
+import Smt.Reconstruction.Certifying.LiftOrNToNeg
 import Smt.Reconstruction.Certifying.PermutateOr
 import Smt.Reconstruction.Certifying.Pull
 import Smt.Reconstruction.Certifying.Quant
 import Smt.Reconstruction.Certifying.Resolution
 import Smt.Reconstruction.Certifying.Util
+
