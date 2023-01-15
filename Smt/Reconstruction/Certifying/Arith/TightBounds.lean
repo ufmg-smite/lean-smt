2a77ebdb4c0ab14dc78b4bc40a012113c2d478ec
tomaz1502 <tomgm1502@gmail.com>
2023-01-15 14:21:19 -0300

    update on tight bounds

--- Smt/Reconstruction/Certifying/Arith/TightBounds.lean
+++ Smt/Reconstruction/Certifying/Arith/TightBounds.lean
@@ -1,15 +1,29 @@
+import Std.Data.Int.Lemmas
 import Std.Data.Rat.Basic
 import Mathlib.Data.Rat.Order
 
+theorem unwrapDecideFalse {p : Prop} [inst : Decidable p] : decide p = false → ¬ p := by
+  intros h
+  unfold decide at h
+  cases inst with
+  | isFalse h' => exact h'
+  | isTrue _   => simp at h
 
-theorem unwrapDecide {p : Prop} [inst : Decidable p] : decide p = true → p := by
+theorem unwrapDecideTrue {p : Prop} [inst : Decidable p] : decide p = true → p := by
   intros h
   unfold decide at h
   cases inst with
   | isFalse _ => simp at h
   | isTrue h' => exact h'
 
-theorem wrapDecide {p : Prop} [inst : Decidable p] : ¬ p → decide p = false := by
+theorem wrapDecideTrue {p : Prop} [inst : Decidable p] : p → decide p = true := by
+  intros h
+  unfold decide
+  cases inst with
+  | isFalse h' => exact absurd h h'
+  | isTrue h' => simp
+
+theorem wrapDecideFalse {p : Prop} [inst : Decidable p] : ¬ p → decide p = false := by
   intros h
   unfold decide
   cases inst with
@@ -25,8 +39,8 @@ theorem castLE : ∀ {a b : Int}, a ≤ b → Rat.ofInt a ≤ Rat.ofInt b := by
   { 
     simp at h₁
     have ⟨h₁f, h₁s⟩ := h₁
-    have bNeg : b < 0 := unwrapDecide h₁f
-    have aNotNeg : 0 ≤ a := unwrapDecide h₁s
+    have bNeg : b < 0 := unwrapDecideTrue h₁f
+    have aNotNeg : 0 ≤ a := unwrapDecideTrue h₁s
     have abs : b < a := lt_of_lt_of_le bNeg aNotNeg
     exact (not_le_of_gt abs) h
   }
@@ -36,17 +50,97 @@ theorem castLE : ∀ {a b : Int}, a ≤ b → Rat.ofInt a ≤ Rat.ofInt b := by
     rw [← bEq0] at h
     simp
     have abs := not_lt_of_ge h
-    exact wrapDecide abs
+    exact wrapDecideFalse abs
   }
   { exact rfl }
   simp
   exact h
 
-theorem floorLtImplies : ∀ {c : Rat} {i : Int}, Rat.floor c < i → Rat.floor c + 1 ≤ i := sorry
+#check Int.div
+#check Int.ediv
 
-theorem floorPlusOneGt : ∀ (c : Rat), c < Rat.ofInt (Rat.floor c + 1) := sorry
+#eval Int.ediv (-5) 2
 
-theorem intTightUb : ∀ {i : Int} {c : Rat}, i < c → i ≤ Rat.floor c := by
+theorem floorLtImplies : ∀ {c : Rat} {i : Int}, Rat.floor c < i → Rat.floor c + 1 ≤ i := by
+  intros c i h
+  exact (Int.lt_iff_add_one_le (Rat.floor c) i).mp h
+
+theorem floorPlusOneGt : ∀ (c : Rat), c < Rat.ofInt (Rat.floor c + 1) := by
+  intro c
+  show Rat.blt c (Rat.ofInt (Rat.floor c + 1))
+
+  unfold Rat.blt
+  split_ifs with h₁ h₂ h₃
+  { exact rfl }
+  {
+    unfold Rat.floor
+    apply wrapDecideTrue
+    rw [h₂]
+    simp
+    split_ifs with h₄
+    { simp }
+    { rw [Int.zero_ediv c.den]; simp  }
+  }
+  {
+    simp at *
+    have ⟨h₄, h₅⟩ := h₃
+    unfold Rat.floor at h₅
+    split_ifs at h₅ with h₆
+    {
+      simp at h₅
+      norm_cast at h₅
+      have h₄' := add_lt_add_right h₄ 1
+      have abs := lt_of_lt_of_le h₄' h₅
+      simp at abs
+    }
+    norm_cast at h₅
+    simp at h₅ 
+    have blah : 0 ≤ c.num / c.den := Int.ediv_nonneg (le_of_lt h₄) (by simp)
+    have bleh := Int.add_le_add_right blah 1
+    have blih := le_trans bleh h₅
+    simp at blih
+  }
+  simp at *
+  unfold Rat.floor
+  split_ifs with h₄
+  {
+    rw [h₄]
+    norm_cast
+    simp
+  }
+  simp
+  norm_cast
+  simp
+  cases c.num with
+  | ofNat num'   =>
+    simp
+    norm_cast
+    suffices c.den * (num' / c.den) + num' % c.den < (num' / c.den + 1) * c.den 
+      by rw [Nat.div_add_mod num' c.den] at this; exact this
+
+    rw [Nat.right_distrib]
+    rw [Nat.mul_comm]
+    apply add_lt_add_left
+    simp
+    apply Nat.mod_lt
+    exact (Nat.zero_lt_of_ne_zero c.den_nz)
+  | negSucc num' =>
+    simp
+    show Int.negSucc num' + 1 ≤ (Int.negSucc num' / c.den + 1) * c.den
+    rw [Int.negSucc_coe num']
+    rw [Int.ofNat_add]
+    simp
+    rw [Int.add_mul]
+
+    simp
+
+    rw [← Int.neg_add]
+
+    admit
+
+#check Int.mul_add
+
+theorem intTightUb : ∀ {i : Int} {c : Rat}, Rat.ofInt i < c → i ≤ Rat.floor c := by
   intros i c h
   cases lt_trichotomy i (Rat.floor c) with
   | inl iltc => exact le_of_lt iltc
