81d0926eaee72496fc9eb8d8a8811c73cacd23e1
tomaz1502 <tomgm1502@gmail.com>
2023-01-24 18:08:57 -0300

    fix on liftorntoneg

--- Smt/Reconstruction/Certifying/LiftOrNToNeg.lean
+++ Smt/Reconstruction/Certifying/LiftOrNToNeg.lean
@@ -1,10 +1,45 @@
 import Lean
 
 import Smt.Reconstruction.Certifying.Boolean
+import Smt.Reconstruction.Certifying.LiftOrNToImp
 import Smt.Reconstruction.Certifying.Util
 
 open Lean Elab Tactic Meta Expr
 open List
+
+theorem orFalse : ∀ {A : Prop}, A ∨ False → A := by
+  intros a h
+  cases h with
+  | inl ha => exact ha
+  | inr ff => exact False.elim ff
+/-
+  removeFalse: takes an or-chain ended in `False` and proves
+  that it implies the same or-chain without the `False` in the end.
+-/
+syntax (name := removeFalse) "removeFalse" term "," term : tactic
+
+def removeFalseCore (hyp hypType : Expr) (stx : Syntax) (out : Name) : TacticM Unit := do
+  let length := getLength hypType
+  let fid: TacticM Ident := do
+    if length > 2 then
+      let fname ← mkFreshId
+      groupOrPrefixCore hyp hypType (length - 1) fname
+      return (mkIdent fname)
+    else return (mkIdent stx[1].getId)
+  let fid ← fid
+  evalTactic (← `(tactic| have $(mkIdent out) := orFalse $fid))
+
+@[tactic removeFalse] def evalRemoveFalse : Tactic :=
+  fun stx => withMainContext do
+    let hyp ← elabTerm stx[1] none
+    let hypType ← inferType hyp
+    let out: Ident := ⟨stx[3]⟩
+    removeFalseCore hyp hypType stx out.getId
+
+example : ¬ A ∨ ¬ B ∨ ¬ C ∨ False → ¬ A ∨ ¬ B ∨ ¬ C := by
+  intro h
+  removeFalse h, h₁
+  exact h₁
 
 syntax (name := liftOrNToNeg) "liftOrNToNeg" term : tactic
 
@@ -12,13 +47,19 @@ syntax (name := liftOrNToNeg) "liftOrNToNeg" term : tactic
   fun stx => withMainContext do
     let hyp ← elabTerm stx[1] none
     let hypType ← inferType hyp
-    let props := map notExpr (collectPropsInOrChain hypType)
-    let propsList: Expr := listExpr props $ Expr.sort Level.zero
-    let deMorgan: Expr := mkApp (mkConst `deMorgan₂) propsList
-    let modusTollens: Expr ← mkAppM `modusTollens #[deMorgan]
-    let notNotHyp: Expr ← mkAppM `notNotIntro #[hyp]
-    let answer := mkApp modusTollens notNotHyp
-    Tactic.closeMainGoal answer
+    let fname ← mkFreshId
+    removeFalseCore hyp hypType stx fname
+    withMainContext do
+      let lctx ← getLCtx
+      let hyp' := (lctx.findFromUserName? fname).get!.toExpr
+      let hypType' ← instantiateMVars (← inferType hyp')
+      let props := map notExpr (collectPropsInOrChain hypType')
+      let propsList: Expr := listExpr props $ Expr.sort Level.zero
+      let deMorgan: Expr := mkApp (mkConst `deMorgan₂) propsList
+      let modusTollens: Expr ← mkAppM `modusTollens #[deMorgan]
+      let notNotHyp: Expr ← mkAppM `notNotIntro #[hyp']
+      let answer := mkApp modusTollens notNotHyp
+      Tactic.closeMainGoal answer
 
 theorem modusTollens {A B : Prop} : (A → B) → ¬ B → ¬ A := by
   intros f nb
@@ -26,8 +67,7 @@ theorem modusTollens {A B : Prop} : (A → B) → ¬ B → ¬ A := by
   | Or.inl ha  => absurd (f ha) nb
   | Or.inr hna => hna
 
-example : ¬ A ∨ ¬ B ∨ ¬ C ∨ ¬ D → ¬ (A ∧ B ∧ C ∧ D) := by
+example : ¬ A ∨ ¬ B ∨ ¬ C ∨ ¬ D ∨ False → ¬ (A ∧ B ∧ C ∧ D) := by
   intro h
   liftOrNToNeg h
---  fun h => modusTollens (@deMorgan₂ [A, B, C, D]) (notNotIntro h)
 
