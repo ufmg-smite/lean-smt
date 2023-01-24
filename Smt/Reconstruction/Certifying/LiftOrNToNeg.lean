130ae54569d8ea740aae6ffb8cfbb56cdb14c719
tomaz1502 <tomgm1502@gmail.com>
2023-01-24 18:13:40 -0300

    using builtin modus tollens

--- Smt/Reconstruction/Certifying/LiftOrNToNeg.lean
+++ Smt/Reconstruction/Certifying/LiftOrNToNeg.lean
@@ -56,16 +56,10 @@ syntax (name := liftOrNToNeg) "liftOrNToNeg" term : tactic
       let props := map notExpr (collectPropsInOrChain hypType')
       let propsList: Expr := listExpr props $ Expr.sort Level.zero
       let deMorgan: Expr := mkApp (mkConst `deMorgan₂) propsList
-      let modusTollens: Expr ← mkAppM `modusTollens #[deMorgan]
+      let modusTollens: Expr ← mkAppM `mt #[deMorgan]
       let notNotHyp: Expr ← mkAppM `notNotIntro #[hyp']
       let answer := mkApp modusTollens notNotHyp
       Tactic.closeMainGoal answer
-
-theorem modusTollens {A B : Prop} : (A → B) → ¬ B → ¬ A := by
-  intros f nb
-  exact match Classical.em A with
-  | Or.inl ha  => absurd (f ha) nb
-  | Or.inr hna => hna
 
 example : ¬ A ∨ ¬ B ∨ ¬ C ∨ ¬ D ∨ False → ¬ (A ∧ B ∧ C ∧ D) := by
   intro h
