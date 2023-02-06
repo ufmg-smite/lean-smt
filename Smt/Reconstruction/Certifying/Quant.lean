2cc88c746ca185a542d4783017008559ca96b856
Haniel Barbosa <hanielbbarbosa@gmail.com>
2023-02-06 17:58:45 -0300

    [quant] Have instForall require body, instantiation
    Also adds a macro to pass premise first and an example with two instantiated
    variables.

--- Smt/Reconstruction/Certifying/Quant.lean
+++ Smt/Reconstruction/Certifying/Quant.lean
@@ -2,8 +2,13 @@ open Classical
 
 universe u
 
-theorem instForAll {α : Sort u} {f : α → Prop} {a : α} :
+theorem instForall {α : Sort u} (f : α → Prop) (a : α) :
   (forall a' : α, f a') → f a := λ h => h a
+
+syntax "flipInstForall " (term)? (term)? (term)? : term
+macro_rules
+| `(flipInstForall $premiss:term $arg₁:term $arg₂:term) =>
+    `(instForall $arg₁ $arg₂ $premiss)
 
 theorem instEqual₁ {A : Type u} {P : A → Prop} {t : A} :
   (forall x : A, x = t → P x) → P t := λ h => h t rfl
@@ -34,3 +39,7 @@ example : ∃ x₁ x₂ : Int, x₁ + x₂ > 0 = (e₁ + e₂ > 0) :=
     (smtSkolem (λ x₁ : Int => ∃ x₂, x₁ + x₂ > 0))
     (smtSkolem (λ x₂ : Int => e₁ + x₂ > 0))
 
+example : (∀ x y : Int, x + y > 0) → (3 : Int) + 4 > 0 := by
+  intro h
+  let s := (instForall (fun x : Int => (forall y : Int, x + y > 0)) 3 h)
+  exact (instForall (fun y : Int => 3 + y > 0) 4 s)
