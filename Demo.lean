5a569c04a0f307d9c1625c4232e1cb8cca76e0a9
Abdalrhman M Mohamed <abdoo8080@outlook.com>
2023-02-13 17:20:55 -0800

    Slight changes to demo.

--- Demo.lean
+++ Demo.lean
@@ -16,17 +16,17 @@ theorem sum_formula : n ≥ 0 → sum n = n * (n + 1) / 2 := by
     | zero => smt [sum]; sorry
     | succ n ih => smt [sum, ih]; sorry
 
-variable (G : Type) [Nonempty G] (op : G → G → G) (inv : G → G) (e : G)
+variable {G : Type} [Nonempty G] (op : G → G → G) (inv : G → G) (e : G)
 
 axiom assoc   : ∀ a b c, op a (op b c) = op (op a b) c
 axiom inverse : ∀ a, op (inv a) a = e
 axiom ident   : ∀ a, op e a = a
 
 theorem inverse' : ∀ a, op a (inv a) = e := by
-  smt [assoc G op, inverse G op inv e, ident G op e]
+  smt [assoc op, inverse op inv e, ident op e]
 
 theorem identity' : ∀ a, op a e = a := by
-  smt [assoc G op, inverse G op inv e, ident G op e, inverse' G op inv e]
+  smt [assoc op, inverse op inv e, ident op e, inverse' op inv e]
 
 theorem unique_identity e' : (∀ z, op e' z = z) → e' = e := by
-  smt [assoc G op, inverse G op inv e, ident G op e]
+  smt [assoc op, inverse op inv e, ident op e]
