6de0c4b216a918a14cf7a47d9a6faccaf8c8a209
Abdalrhman Mohamed <abdoo8080@outlook.com>
2023-03-23 13:36:26 -0700

    Add more rewrite rules and fix CI. (#56)

--- Test/Bool/PropExt.lean
+++ Test/Bool/PropExt.lean
@@ -2,5 +2,5 @@ import Smt
 
 theorem prop_ext (p q : Bool) : (p ↔ q) → p == q := by
   smt
-  intro hpq
-  cases hpq <;> cases p <;> cases q <;> simp_all
+  intro ⟨hpq, hqp⟩
+  cases p <;> cases q <;> simp_all
