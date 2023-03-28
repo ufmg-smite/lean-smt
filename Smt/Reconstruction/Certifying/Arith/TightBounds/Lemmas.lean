f0b1caf9ebbb91cdbe9d5397fe39ac7bf043dca0
tomaz1502 <tomgm1502@gmail.com>
2023-03-28 19:17:49 -0300

    adding copyright notice

--- Smt/Reconstruction/Certifying/Arith/TightBounds/Lemmas.lean
+++ Smt/Reconstruction/Certifying/Arith/TightBounds/Lemmas.lean
@@ -1,3 +1,10 @@
+/-
+Copyright (c) 2021-2023 by the authors listed in the file AUTHORS and their
+institutional affiliations. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Tomaz Gomes Mascarenhas
+-/
+
 import Mathlib.Data.Rat.Floor
 import Mathlib.Algebra.Order.Floor
 
--- Smt/Reconstruction/Certifying/Arith/Trichotomy.lean
+++ Smt/Reconstruction/Certifying/Arith/Trichotomy.lean
@@ -1,3 +1,10 @@
+/-
+Copyright (c) 2021-2023 by the authors listed in the file AUTHORS and their
+institutional affiliations. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Tomaz Gomes Mascarenhas
+-/
+
 -- implementation of:
 -- https://cvc5.github.io/docs/cvc5-1.0.2/proofs/proof_rules.html#_CPPv4N4cvc58internal6PfRule16ARITH_TRICHOTOMYE
 
