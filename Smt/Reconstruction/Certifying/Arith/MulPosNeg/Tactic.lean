ee2e144db44e65be17bb8cb94daa8ab461a14fc7
tomaz1502 <tomgm1502@gmail.com>
2023-04-13 19:27:50 -0300

    changing interface for arithMulPosNeg

--- Smt/Reconstruction/Certifying/Arith/MulPosNeg/Tactic.lean
+++ Smt/Reconstruction/Certifying/Arith/MulPosNeg/Tactic.lean
@@ -5,7 +5,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Tomaz Gomes Mascarenhas
 -/
 
+import Smt.Reconstruction.Certifying.Arith.MulPosNeg.Instances
 import Smt.Reconstruction.Certifying.Arith.MulPosNeg.Lemmas
+import Smt.Reconstruction.Certifying.Util
 
 import Mathlib.Data.Rat.Order
 import Mathlib.Data.Int.Order.Basic
@@ -15,46 +17,63 @@ import Lean
 namespace Smt.Reconstruction.Certifying
 
 open Lean hiding Rat
-open Elab.Tactic Expr Meta
+open Elab Tactic Expr Meta
 
-syntax (name := arithMulPos) "arithMulPos" term "," term : tactic
+syntax (name := arithMulPos) "arithMulPos" ("[" term,* "]")? "," term : tactic
+syntax (name := arithMulNeg) "arithMulNeg" ("[" term,* "]")? "," term : tactic
+
+def parseArithMulAux : Array Term → Term → TacticM (List Name × Nat)
+  | hs, i => do
+    let li: List Name :=
+      hs.toList.map (fun h: Term =>
+                       let hIdent : Ident := ⟨h⟩
+                       hIdent.getId
+                    )
+    let i' ← stxToNat i
+    return (li, i')
+
+def parseArithMul : Syntax → TacticM (List Name × Nat)
+  | `(tactic| arithMulPos [ $[$hs],* ], $i) => parseArithMulAux hs i
+  | `(tactic| arithMulNeg [ $[$hs],* ], $i) => parseArithMulAux hs i
+  | _ => throwError "[arithMul]: wrong usage"
+
+def evalArithMulAux : Syntax → List Name → TacticM Unit:=
+  fun stx thms =>
+    withMainContext do
+      let (names, compId) ← parseArithMul stx
+      let (a, b, c) ←
+        match names with
+        | [a', b', c'] => pure (a', b', c')
+        | _            => throwError "[arithMulNeg]: List must have 3 elements"
+      let lctx ← getLCtx
+      let va := (lctx.findFromUserName? a).get!.toExpr
+      let vb := (lctx.findFromUserName? b).get!.toExpr
+      let vc := (lctx.findFromUserName? c).get!.toExpr
+      let type ← inferType va
+      let thmName: Name ←
+        if compId <= 3 then
+          pure (thms.get! compId) 
+        else throwError "[arithMul]: unexpected second argument"
+      let inst ←
+        match type with
+        | const `Int .. => pure $ mkConst ``lorInt
+        | const `Rat .. => pure $ mkConst ``lorRat
+        | _ => throwError "[arithMulNeg]: unexpected type for variable"
+      let proof := mkApp5 (mkConst thmName) type inst va vb vc
+      closeMainGoal proof
+
 @[tactic arithMulPos] def evalArithMulPos : Tactic := fun stx =>
-  withMainContext do
-    let h₁ ← elabTerm stx[1] none
-    let h₂ ← elabTerm stx[3] none
-    let t₁ ← Meta.inferType h₁
-    let thmName ← getThmName t₁
-    let proof : Expr ← mkAppM thmName #[h₁, h₂]
-    closeMainGoal proof
-where getThmName : Expr → TacticM Name
-        | app (app (app (app (app (const nm ..) ..) ..) ..) ..) _ => matchNameThm nm
-        | app (app (app (app (const nm ..) ..) ..) ..) _ => matchNameThm nm
-        | _ => throwError "[arithMulPos]: structure not supported"
-      matchNameThm : Name → TacticM Name
-        | ``LE.le => pure ``arith_mul_pos_le
-        | ``LT.lt => pure ``arith_mul_pos_lt
-        | ``GT.gt => pure ``arith_mul_pos_gt
-        | ``GE.ge => pure ``arith_mul_pos_ge
-        | _ => throwError "[arithMulPos]: invalid comparator"
+  evalArithMulAux stx [ ``arith_mul_pos_lt
+                      , ``arith_mul_pos_le
+                      , ``arith_mul_pos_gt
+                      , ``arith_mul_pos_ge
+                      ]
 
-syntax (name := arithMulNeg) "arithMulNeg" term "," term : tactic
 @[tactic arithMulNeg] def evalArithMulNeg : Tactic := fun stx =>
-  withMainContext do
-    let h₁ ← elabTerm stx[1] none
-    let h₂ ← elabTerm stx[3] none
-    let t₁ ← Meta.inferType h₁
-    let thmName ← getThmName t₁
-    let proof : Expr ← mkAppM thmName #[h₁, h₂]
-    closeMainGoal proof
-where getThmName : Expr → TacticM Name
-        | app (app (app (app (app (const nm ..) ..) ..) ..) ..) _ => matchNameThm nm
-        | app (app (app (app (const nm ..) ..) ..) ..) _ => matchNameThm nm
-        | _ => throwError "[arithMulNeg]: structure not supported"
-      matchNameThm : Name → TacticM Name
-        | ``LE.le => pure ``arith_mul_neg_le
-        | ``LT.lt => pure ``arith_mul_neg_lt
-        | ``GT.gt => pure ``arith_mul_neg_gt
-        | ``GE.ge => pure ``arith_mul_neg_ge
-        | _ => throwError "[arithMulNeg]: invalid comparator"
+  evalArithMulAux stx [ ``arith_mul_neg_lt
+                      , ``arith_mul_neg_le
+                      , ``arith_mul_neg_gt
+                      , ``arith_mul_neg_ge
+                      ]
 
 end Smt.Reconstruction.Certifying
--- Test/Reconstruction/Arith/MulPosNeg.lean
+++ Test/Reconstruction/Arith/MulPosNeg.lean
@@ -3,9 +3,8 @@ import Smt
 open Smt.Reconstruction.Certifying
 
 example {a b c : Int} : a < b → 0 < c → c * a < c * b := by
-  intros h₁ h₂
-  arithMulPos h₁, h₂
+  arithMulPos [a,b,c], 0
 
 example {a b c : ℚ} : a ≤ b → 0 > c → c * a ≥ c * b := by
-  intros h₁ h₂
-  arithMulNeg h₁, h₂
+  arithMulNeg [a,b,c], 1
+
