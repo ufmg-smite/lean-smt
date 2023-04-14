e866689e9e681d08f79e14b13ef01a10fc7b5d53
tomaz1502 <tomgm1502@gmail.com>
2023-04-14 11:51:06 -0300

    porting tightBounds to MetaM

--- Smt/Reconstruction/Certifying/Arith/TightBounds/Tactic.lean
+++ Smt/Reconstruction/Certifying/Arith/TightBounds/Tactic.lean
@@ -15,34 +15,41 @@ open Meta Elab.Tactic Expr
 
 namespace Smt.Reconstruction.Certifying
 
+def isIntLt : Expr → Bool
+  | app (app (app (app _ (const ``Int ..)) ..) ..) .. => true
+  | _ => false
+
+def intTightMeta (mvar : MVarId) (h : Expr) (thmName outName : Name)
+  : MetaM MVarId :=
+  mvar.withContext do
+    let t ← inferType h
+    let arg ←
+      if isIntLt t then
+        mkAppM ``castLT #[h]
+      else pure h
+    let answer ← mkAppM thmName #[arg]
+    let answerType ← inferType answer
+    let (_, mvar') ← MVarId.intro1P $ ← mvar.assert outName answerType answer 
+    return mvar'
+
 syntax (name := intTightUb) "intTightUb" term : tactic
 @[tactic intTightUb] def evalIntTightUb : Tactic := fun stx =>
   withMainContext do
     let h ← elabTerm stx[1] none
-    let t ← inferType h
-    let hStx := ⟨stx[1]⟩
-    if isIntLt t then
-      evalTactic (← `(tactic| exact intTightUb' (castLT $hStx)))
-    else
-      evalTactic (← `(tactic| exact intTightUb' $hStx))
-where
-  isIntLt : Expr → Bool
-  | app (app (app (app _ (const ``Int ..)) ..) ..) .. => True
-  | _ => False
+    let fname ← mkFreshId
+    let mvar ← getMainGoal
+    let mvar' ← intTightMeta mvar h ``intTightUb' fname
+    replaceMainGoal [mvar']
+    evalTactic (← `(tactic| exact $(mkIdent fname)))
 
 syntax (name := intTightLb) "intTightLb" term : tactic
 @[tactic intTightLb] def evalIntTightLb : Tactic := fun stx =>
   withMainContext do
     let h ← elabTerm stx[1] none
-    let t ← inferType h
-    let hStx := ⟨stx[1]⟩
-    if isIntLt t then
-      evalTactic (← `(tactic| exact intTightLb' (castLT $hStx)))
-    else
-      evalTactic (← `(tactic| exact intTightLb' $hStx))
-where
-  isIntLt : Expr → Bool
-  | app (app (app (app _ (const ``Int ..)) ..) ..) .. => True
-  | _ => False
+    let fname ← mkFreshId
+    let mvar ← getMainGoal
+    let mvar' ← intTightMeta mvar h ``intTightLb' fname
+    replaceMainGoal [mvar']
+    evalTactic (← `(tactic| exact $(mkIdent fname)))
 
 end Smt.Reconstruction.Certifying
