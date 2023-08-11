71305a4039fd94bb6dc1aa6ddacb393e63bc3cbd
tomaz1502 <tomgm1502@gmail.com>
2023-08-10 23:53:59 -0300

    fix test on mulposneg

--- Test/Reconstruction/Arith/MulPosNeg.lean
+++ Test/Reconstruction/Arith/MulPosNeg.lean
@@ -2,16 +2,14 @@ import Smt
 
 open Smt.Reconstruction.Certifying
 
-example {a b c : Int} : a < b ∧ c > 0 → c * a < c * b := sorry
-
-example {a b c : Int} : a < b → c > 0 → c * a < c * b := by
+example {a b c : Int} : c > 0 ∧ a < b → c * a < c * b := by
   arithMulPos [a,b,c], 0
 
-example {a b c : ℚ} : a ≤ b → 0 > c → c * a ≥ c * b := by
+example {a b c : ℚ} : 0 > c ∧ a ≤ b → c * a ≥ c * b := by
   arithMulNeg [a,b,c], 1
 
-example {a b c : Int} : a < b → 0 < 2 * c → (2 * c) * a < (2 * c) * b := by
+example {a b c : Int} : 0 < 2 * c ∧ a < b → (2 * c) * a < (2 * c) * b := by
   arithMulPos [a, b, 2 * c], 0
 
-example {a b : Int} : a < b → 0 < (2 : Int) → 2 * a < 2 * b := by
+example {a b : Int} : 0 < (2 : Int) ∧ a < b → 2 * a < 2 * b := by
   arithMulPos [a,b,2], 0
