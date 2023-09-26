6ff9e9aabc468b1a073a26e66c9d24d682fa589d
Abdalrhman Mohamed <abdoo8080@outlook.com>
2023-09-26 15:36:01 -0700

    Prove remaining rewrite rules for the supported theories. (#62)
    This PR updates the rewrite rules used by cvc5 for builtin, boolean, and UF theories. Additionally, it reproves the arithmetic rewrite rules to work for both integers and rational numbers.

--- Smt/Reconstruction/Rewrites/Arith.lean
+++ Smt/Reconstruction/Rewrites/Arith.lean
@@ -5,73 +5,79 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Abdalrhman Mohamed
 -/
 
-import Std.Data.Int.Lemmas
+import Mathlib.Algebra.Order.Ring.Defs
 import Smt.Reconstruction.Rewrites.Simp
 
 namespace Smt.Reconstruction.Rewrites.Arith
 
-scoped notation "const " c => fun _ => c
+-- https://github.com/cvc5/cvc5/blob/proof-new/src/theory/arith/rewrites
 
-variable {xs w ys zs t x s r : Int}
+open Function
 
--- Basic Rules
+variable {α : Type} [LinearOrderedRing α]
+
+variable {xs w ys zs t x s r : α}
 
 @[smt_simp] theorem arith_plus_zero : t + 0 + s = t + s :=
-  t.add_zero.symm ▸ rfl
-
+  (add_zero t).symm ▸ rfl
 @[smt_simp] theorem arith_mul_one : t * 1 * s = t * s :=
-  t.mul_one.symm ▸ rfl
+  (mul_one t).symm ▸ rfl
 @[smt_simp] theorem arith_mul_zero : t * 0 * s = 0 :=
-  t.mul_zero.symm ▸ s.zero_mul.symm ▸ rfl
+  (mul_zero t).symm ▸ (zero_mul s).symm ▸ rfl
 
-@[smt_simp] theorem arith_uminus_elim : -t = -1 * t :=
-  t.neg_eq_neg_one_mul
+@[smt_simp] theorem arith_int_div_one {t : Int} : t / 1 = t :=
+  Int.ediv_one t
+
 @[smt_simp] theorem arith_neg_neg_one : -1 * (-1 * t) = t :=
-  Int.mul_assoc (-1) (-1) t ▸ t.one_mul.symm ▸ rfl
+  neg_mul _ t ▸ (one_mul t).symm ▸ neg_mul_neg _ t ▸ (one_mul t).symm ▸ rfl
 
 -- Eliminations
 
+@[smt_simp] theorem arith_elim_uminus : -t = -1 * t :=
+  neg_eq_neg_one_mul t ▸ rfl
 @[smt_simp] theorem arith_elim_minus : t - s = t + -1 * s :=
-  s.neg_eq_neg_one_mul ▸ rfl
+  neg_eq_neg_one_mul s ▸ sub_eq_add_neg _ s ▸ rfl
 @[smt_simp] theorem arith_elim_gt : (t > s) = ¬(t ≤ s) :=
-  propext Int.not_le.symm
+  propext not_le.symm
 @[smt_simp] theorem arith_elim_lt : (t < s) = ¬(t ≥ s) :=
-  propext Int.not_le.symm
-@[smt_simp] theorem arith_elim_leq : (t ≤ s) = ¬(t ≥ s + 1) :=
-  propext Int.not_lt.symm
+  propext not_le.symm
+@[smt_simp] theorem arith_elim_leq : (t ≤ s) = (s ≥ t) :=
+  propext ge_iff_le
 
-@[smt_simp] theorem arith_geq_tighten : (¬(t ≥ s)) = (s ≥ t + 1) :=
+@[smt_simp] theorem arith_leq_norm {t s : Int}: (t ≤ s) = ¬(t ≥ s + 1) :=
+  propext ⟨(propext Int.not_le ▸ Int.lt_add_one_of_le ·),
+           (Int.le_of_lt_add_one $ propext Int.not_le ▸ · )⟩
+
+@[smt_simp] theorem arith_geq_tighten {t s : Int} : (¬(t ≥ s)) = (s ≥ t + 1) :=
   propext Int.not_le
 
 @[smt_simp] theorem arith_geq_norm: (t ≥ s) = (t - s ≥ 0) :=
-  propext ⟨Int.sub_nonneg_of_le, Int.le_of_sub_nonneg⟩
+  propext ⟨sub_nonneg_of_le, le_of_sub_nonneg⟩
 
 @[smt_simp] theorem arith_refl_leq : (t ≤ t) = True :=
-  propext ⟨const trivial, const t.le_refl⟩
+  propext ⟨const _ trivial, const _ (le_refl t)⟩
 @[smt_simp] theorem arith_refl_lt : (t < t) = False :=
-  propext ⟨t.lt_irrefl, False.elim⟩
+  propext ⟨(lt_irrefl t), False.elim⟩
 @[smt_simp] theorem arith_refl_geq : (t ≥ t) = True :=
-  propext ⟨const trivial, const t.le_refl⟩
+  propext ⟨const _ trivial, const _ (le_refl t)⟩
 @[smt_simp] theorem arith_refl_gt : (t > t) = False :=
-  propext ⟨t.lt_irrefl, False.elim⟩
+  propext ⟨(lt_irrefl t), False.elim⟩
 
-@[smt_simp] theorem plus_flatten : xs + (w + ys) + zs = xs + w + ys + zs :=
-  Int.add_assoc xs w ys ▸ rfl
+@[smt_simp] theorem arith_plus_flatten : xs + (w + ys) + zs = xs + w + ys + zs :=
+  add_assoc xs w ys ▸ rfl
 
-@[smt_simp] theorem mult_flatten : xs * (w * ys) * zs = xs * w * ys * zs :=
-  Int.mul_assoc xs w ys ▸ rfl
+@[smt_simp] theorem arith_mult_flatten : xs * (w * ys) * zs = xs * w * ys * zs :=
+  mul_assoc xs w ys ▸ rfl
 
-@[smt_simp] theorem mult_dist : x * (y + z + w) = x * y + x * (z + w) :=
-  Int.add_assoc y z w ▸ Int.mul_add x y (z + w) ▸ rfl
-
--- TEMPORARY
+@[smt_simp] theorem arith_mult_dist : x * (y + z + w) = x * y + x * (z + w) :=
+  add_assoc y z w ▸ mul_add x y (z + w) ▸ rfl
 
 @[smt_simp] theorem arith_plus_cancel1 : t + x + s + (-1 * x) + r = t + s + r :=
-  arith_uminus_elim ▸ Int.add_assoc t x s ▸ Int.add_assoc t (x + s) (-x) ▸
-  Int.add_comm x s ▸ (Int.add_neg_cancel_right s x).symm ▸ rfl
+  neg_eq_neg_one_mul x ▸ add_assoc t x s ▸ add_assoc t (x + s) (-x) ▸
+  add_comm x s ▸ (add_neg_cancel_right s x).symm ▸ rfl
 
 @[smt_simp] theorem arith_plus_cancel2 : t + (-1 * x) + s + x + r = t + s + r :=
-  arith_uminus_elim ▸ Int.add_assoc t (-x) s ▸ Int.add_assoc t (-x + s) x ▸
-  Int.add_comm (-x) s ▸ (Int.neg_add_cancel_right s x).symm ▸ rfl
+  neg_eq_neg_one_mul x ▸ add_assoc t (-x) s ▸ add_assoc t (-x + s) x ▸
+  add_comm (-x) s ▸ (neg_add_cancel_right s x).symm ▸ rfl
 
 end Smt.Reconstruction.Rewrites.Arith
--- Test/EUF/Example.lean
+++ Test/EUF/Example.lean
@@ -2,4 +2,5 @@ import Smt
 
 theorem euf {U : Type} [Nonempty U] {f : U → U → U} {a b c d : U} : (a = b) → (c = d) → p1 ∧ True → (¬ p1) ∨ (p2 ∧ p3) → (¬ p3) ∨ (¬ (f a c = f b d)) → False := by
   smt
-  exact (and_true p1).symm
+  · exact propext ⟨Or.inl, (Or.elim · id False.elim)⟩
+  · exact (and_true p1).symm
