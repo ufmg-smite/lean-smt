91b3de0c83f8c58cdf6c61c87f97e2ac452f1cff
Kaiyu Yang <kaiyuy@caltech.edu>
2023-11-13 19:46:09 -0800

    add more testing examples from minif2f

--- Test/MiniF2F/Valid.lean
+++ Test/MiniF2F/Valid.lean
@@ -69,3 +69,66 @@ set_option autoImplicit false
 theorem amc12a_2015_p10 (x y : ℤ) (h₀ : 0 < y) (h₁ : y < x) (h₂ : x + y + x * y = 80) : x = 26 := by
   smt [h₀, h₁, h₂]
   sorry
+
+theorem mathd_numbertheory_780 (m x : ℕ) (h₀ : 10 ≤ m) (h₁ : m ≤ 99) (h₂ : 6 * x % m = 1)
+    (h₃ : (x - 6 ^ 2) % m = 0) : m = 43 := by
+  smt [h₀, h₁, h₂, h₃]
+  sorry
+
+theorem amc12a_2009_p9 (a b c : ℝ) (f : ℝ → ℝ) (h₀ : ∀ x, f (x + 3) = 3 * x ^ 2 + 7 * x + 4)
+    (h₁ : ∀ x, f x = a * x ^ 2 + b * x + c) : a + b + c = 2 := by
+  smt [h₀, h₁]
+
+theorem mathd_algebra_13 (a b : ℝ)
+    (h₀ : ∀ x, x - 3 ≠ 0 ∧ x - 5 ≠ 0 → 4 * x / (x ^ 2 - 8 * x + 15) = a / (x - 3) + b / (x - 5)) :
+    a = -6 ∧ b = 10 := by
+  smt [h₀]
+  sorry
+
+theorem imo_1984_p2 (a b : ℕ) (h₀ : 0 < a ∧ 0 < b) (h₁ : ¬7 ∣ a) (h₂ : ¬7 ∣ b) (h₃ : ¬7 ∣ a + b)
+    (h₄ : 7 ^ 7 ∣ (a + b) ^ 7 - a ^ 7 - b ^ 7) : 19 ≤ a + b := by
+  smt [h₀, h₁, h₂, h₃, h₄]
+  sorry
+
+theorem mathd_algebra_267 (x : ℝ) (h₀ : x ≠ 1) (h₁ : x ≠ -2)
+    (h₂ : (x + 1) / (x - 1) = (x - 2) / (x + 2)) : x = 0 := by
+  smt [h₀, h₁, h₂]
+  sorry
+
+theorem induction_seq_mul2pnp1 (n : ℕ) (u : ℕ → ℕ) (h₀ : u 0 = 0)
+    (h₁ : ∀ n, u (n + 1) = 2 * u n + (n + 1)) : u n = 2 ^ (n + 1) - (n + 2) := by smt [h₀, h₁]
+
+theorem imo_1977_p5 (a b q r : ℕ) (h₀ : r < a + b) (h₁ : a ^ 2 + b ^ 2 = (a + b) * q + r)
+    (h₂ : q ^ 2 + r = 1977) :
+    abs ((a : ℤ) - 22) = 15 ∧ abs ((b : ℤ) - 22) = 28 ∨
+      abs ((a : ℤ) - 22) = 28 ∧ abs ((b : ℤ) - 22) = 15 := by
+  smt [h₀, h₁, h₂]
+  sorry
+
+theorem amc12a_2010_p11 (x b : ℝ) (h₀ : 0 < b) (h₁ : (7 : ℝ) ^ (x + 7) = 8 ^ x)
+    (h₂ : x = Real.logb b (7 ^ 7)) : b = 8 / 7 := by
+  smt [h₀, h₁, h₂]
+  sorry
+
+theorem mathd_algebra_206 (a b : ℝ) (f : ℝ → ℝ) (h₀ : ∀ x, f x = x ^ 2 + a * x + b) (h₁ : 2 * a ≠ b)
+    (h₂ : f (2 * a) = 0) (h₃ : f b = 0) : a + b = -1 := by
+  smt [h₀, h₁, h₂, h₃]
+  sorry
+
+theorem mathd_numbertheory_412 (x y : ℕ) (h₀ : x % 19 = 4) (h₁ : y % 19 = 7) :
+    (x + 1) ^ 2 * (y + 5) ^ 3 % 19 = 13 := by
+  smt [h₀, h₁]
+  sorry
+
+theorem mathd_algebra_247 (t s : ℝ) (n : ℤ) (h₀ : t = 2 * s - s ^ 2) (h₁ : s = n ^ 2 - 2 ^ n + 1) (h₂ : n = 3) : t = 0 := by
+  smt [h₀, h₁, h₂]
+  sorry
+
+theorem algebra_sqineq_2unitcircatblt1 (a b : ℝ) (h₀ : a ^ 2 + b ^ 2 = 2) : a * b ≤ 1 := by
+  smt [h₀]
+  sorry
+
+theorem algebra_amgm_sumasqdivbsqgeqsumbdiva (a b c : ℝ) (h₀ : 0 < a ∧ 0 < b ∧ 0 < c) :
+    a ^ 2 / b ^ 2 + b ^ 2 / c ^ 2 + c ^ 2 / a ^ 2 ≥ b / a + c / b + a / c := by
+  smt [h₀]
+  sorry
