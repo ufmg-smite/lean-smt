3e7268f4e95b9ad1aa4bc2bfcaa0ee484415a72c
Abdalrhman Mohamed <abdoo8080@outlook.com>
2024-05-06 15:25:26 -0700

    Support reconstruction of more BV terms. (#91)

--- Smt/Reconstruct/BitVec.lean
+++ Smt/Reconstruct/BitVec.lean
@@ -8,6 +8,12 @@ Authors: Abdalrhman Mohamed
 import Smt.Reconstruct.Prop.Core
 import Smt.Reconstruct
 
+def Std.Range.foldlM [Monad m] (f : α → Nat → m α) (r : Range) (init : α) : m α := do
+  let mut a := init
+  for i in r do
+    a ← f a i
+  return a
+
 namespace Smt.Reconstruct.BitVec
 
 open Lean Qq
@@ -18,42 +24,7 @@ open Lean Qq
     return q(BitVec $w)
   | _             => return none
 
-@[smt_term_reconstruct] partial def reconstructBitVec : TermReconstructor := fun t => do match t.getKind with
-  | .CONST_BITVECTOR =>
-    let w : Nat := t.getSort.getBitVectorSize.val
-    let v : Nat := (t.getBitVectorValue 10).toNat!
-    return q(BitVec.ofNat $w $v)
-  | .BITVECTOR_BITOF =>
-    let w : Nat := t[0]!.getSort.getBitVectorSize.val
-    let x : Q(BitVec $w) ← reconstructTerm t[0]!
-    let i : Nat := t.getOp[0]!.getIntegerValue.toNat
-    return q(«$x».getLsb $i = true)
-  | .BITVECTOR_BB_TERM =>
-    let w : Nat := t.getNumChildren
-    let bs : Q(BitVec 0) := q(.nil)
-    let f (bs : Expr) (i : Nat) : ReconstructM Expr := do
-      let p : Q(Prop) ← reconstructTerm t[i]!
-      let bs : Q(BitVec $i) := bs
-      let hp : Q(Decidable $p) ← synthDecidableInst t[i]!
-      return q(@BitVec.cons $i (@decide $p $hp) $bs)
-    let bs : Q(BitVec $w) ← (List.range w).foldlM f bs
-    return q($bs)
-  | .BITVECTOR_ADD =>
-    let w : Nat := t.getSort.getBitVectorSize.val
-    let x : Q(BitVec $w) ← reconstructTerm t[0]!
-    let y : Q(BitVec $w) ← reconstructTerm t[1]!
-    return q($x + $y)
-  | _ => return none
-where
-  rightAssocOpDecidableInst (op : Expr) (inst : Expr) (t : cvc5.Term) : ReconstructM Expr := do
-    let mut curr ← reconstructTerm t[t.getNumChildren - 1]!
-    let mut currInst ← synthDecidableInst t[t.getNumChildren - 1]!
-    for i in [1:t.getNumChildren] do
-      let ct := t[t.getNumChildren - i - 1]!
-      currInst := mkApp4 inst (← reconstructTerm ct) curr (← synthDecidableInst ct) currInst
-      curr := mkApp2 op (← reconstructTerm ct) curr
-    return currInst
-  synthDecidableInst (t : cvc5.Term) : ReconstructM Expr := do match t.getKind with
+partial def synthDecidableInst (t : cvc5.Term) : ReconstructM Expr := do match t.getKind with
     | .CONST_BOOLEAN => return if t.getBooleanValue then q(instDecidableTrue) else q(instDecidableFalse)
     | .NOT =>
       let p : Q(Prop) ← reconstructTerm t[0]!
@@ -82,6 +53,188 @@ where
     | _ =>
       let p : Q(Prop) ← reconstructTerm t
       Meta.synthInstance q(Decidable $p)
+where
+   rightAssocOpDecidableInst (op : Expr) (inst : Expr) (t : cvc5.Term) : ReconstructM Expr := do
+    let mut curr ← reconstructTerm t[t.getNumChildren - 1]!
+    let mut currInst ← synthDecidableInst t[t.getNumChildren - 1]!
+    for i in [1:t.getNumChildren] do
+      let ct := t[t.getNumChildren - i - 1]!
+      currInst := mkApp4 inst (← reconstructTerm ct) curr (← synthDecidableInst ct) currInst
+      curr := mkApp2 op (← reconstructTerm ct) curr
+    return currInst
+
+@[smt_term_reconstruct] def reconstructBitVec : TermReconstructor := fun t => do match t.getKind with
+  | .CONST_BITVECTOR =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let v : Nat := (t.getBitVectorValue 10).toNat!
+    return q(BitVec.ofNat $w $v)
+  | .BITVECTOR_BB_TERM =>
+    let w : Nat := t.getNumChildren
+    let bs : Q(BitVec 0) := q(.nil)
+    let f (bs : Expr) (i : Nat) : ReconstructM Expr := do
+      let p : Q(Prop) ← reconstructTerm t[i]!
+      let bs : Q(BitVec $i) := bs
+      let hp : Q(Decidable $p) ← synthDecidableInst t[i]!
+      return q(@BitVec.cons $i (@decide $p $hp) $bs)
+    let bs : Q(BitVec $w) ← (List.range w).foldlM f bs
+    return q($bs)
+  | .BITVECTOR_CONCAT =>
+    let n : Nat := t.getNumChildren
+    let w₁ : Nat := t[0]!.getSort.getBitVectorSize.val
+    let a : Q(BitVec $w₁) ← reconstructTerm t[0]!
+    let f := fun ⟨w₁, a⟩ i => do
+      let w₂ : Nat := t[i]!.getSort.getBitVectorSize.val
+      let x : Q(BitVec $w₂) ← reconstructTerm t[i]!
+      return ⟨q($w₁ + $w₂), q($a ++ $x)⟩
+    let ⟨_, a⟩ ← [1:n].foldlM f (⟨q($w₁), a⟩ : Σ w : Q(Nat), Q(BitVec $w))
+    return a
+  | .BITVECTOR_AND =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    leftAssocOp q(@AndOp.and (BitVec $w) _) t
+  | .BITVECTOR_OR =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    leftAssocOp q(@OrOp.or (BitVec $w) _) t
+  | .BITVECTOR_XOR =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    leftAssocOp q(@Xor.xor (BitVec $w) _) t
+  | .BITVECTOR_NOT =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    return q(~~~$x)
+  | .BITVECTOR_MULT =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    leftAssocOp q(@HMul.hMul (BitVec $w) (BitVec $w) (BitVec $w) _) t
+  | .BITVECTOR_ADD =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    leftAssocOp q(@HAdd.hAdd (BitVec $w) (BitVec $w) (BitVec $w) _) t
+  | .BITVECTOR_SUB =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    leftAssocOp q(@HSub.hSub (BitVec $w) (BitVec $w) (BitVec $w) _) t
+  | .BITVECTOR_NEG =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    return q(-$x)
+  | .BITVECTOR_UDIV =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    let y : Q(BitVec $w) ← reconstructTerm t[1]!
+    return q(BitVec.smtUDiv $x $y)
+  | .BITVECTOR_UREM =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    let y : Q(BitVec $w) ← reconstructTerm t[1]!
+    return q($x % $y)
+  | .BITVECTOR_SDIV =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    let y : Q(BitVec $w) ← reconstructTerm t[1]!
+    return q(BitVec.smtSDiv $x $y)
+  | .BITVECTOR_SREM =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    let y : Q(BitVec $w) ← reconstructTerm t[1]!
+    return q(BitVec.srem $x $y)
+  | .BITVECTOR_SMOD =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    let y : Q(BitVec $w) ← reconstructTerm t[1]!
+    return q(BitVec.smod $x $y)
+  | .BITVECTOR_SHL =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    let i : Nat := t.getOp[0]!.getIntegerValue.toNat
+    return q($x <<< $i)
+  | .BITVECTOR_LSHR =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    let i : Nat := t.getOp[0]!.getIntegerValue.toNat
+    return q($x >>> $i)
+  | .BITVECTOR_ASHR =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    let i : Nat := t.getOp[0]!.getIntegerValue.toNat
+    return q(BitVec.sshiftRight $x $i)
+  | .BITVECTOR_ULT =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    let y : Q(BitVec $w) ← reconstructTerm t[1]!
+    return q($x < $y)
+  | .BITVECTOR_ULE =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    let y : Q(BitVec $w) ← reconstructTerm t[1]!
+    return q($x ≤ $y)
+  | .BITVECTOR_UGT =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    let y : Q(BitVec $w) ← reconstructTerm t[1]!
+    return q($x > $y)
+  | .BITVECTOR_UGE =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    let y : Q(BitVec $w) ← reconstructTerm t[1]!
+    return q($x ≥ $y)
+  | .BITVECTOR_SLT =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    let y : Q(BitVec $w) ← reconstructTerm t[1]!
+    return q(BitVec.slt $x $y)
+  | .BITVECTOR_SLE =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    let y : Q(BitVec $w) ← reconstructTerm t[1]!
+    return q(BitVec.sle $x $y)
+  | .BITVECTOR_EXTRACT =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let i : Nat := t.getOp[0]!.getIntegerValue.toNat
+    let j : Nat := t.getOp[1]!.getIntegerValue.toNat
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    return q(«$x».extractLsb $i $j)
+  | .BITVECTOR_BITOF =>
+    let w : Nat := t[0]!.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    let i : Nat := t.getOp[0]!.getIntegerValue.toNat
+    return q(«$x».getLsb $i = true)
+  | .BITVECTOR_REPEAT =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let n : Nat := t.getOp[0]!.getIntegerValue.toNat
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    return q(«$x».replicate $n)
+  | .BITVECTOR_ZERO_EXTEND =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let n : Nat := t.getOp[0]!.getIntegerValue.toNat
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    return q(«$x».zeroExtend $n)
+  | .BITVECTOR_SIGN_EXTEND =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let n : Nat := t.getOp[0]!.getIntegerValue.toNat
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    return q(«$x».signExtend $n)
+  | .BITVECTOR_ROTATE_LEFT =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let n : Nat := t.getOp[0]!.getIntegerValue.toNat
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    return q(«$x».rotateLeft $n)
+  | .BITVECTOR_ROTATE_RIGHT =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let n : Nat := t.getOp[0]!.getIntegerValue.toNat
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    return q(«$x».rotateRight $n)
+  | .INT_TO_BITVECTOR =>
+    let w : Nat := t.getSort.getBitVectorSize.val
+    let x : Q(Int) ← reconstructTerm t[0]!
+    return q(BitVec.ofNat $w «$x».toNat)
+  | .BITVECTOR_TO_NAT =>
+    let w : Nat := t[0]!.getSort.getBitVectorSize.val
+    let x : Q(BitVec $w) ← reconstructTerm t[0]!
+    return q(«$x».toNat)
+  | _ => return none
+where
+  leftAssocOp (op : Expr) (t : cvc5.Term) : ReconstructM Expr := do
+    let mut curr ← reconstructTerm t[0]!
+    for i in [1:t.getNumChildren] do
+      curr := mkApp2 op curr (← reconstructTerm t[i]!)
+    return curr
 
 def reconstructRewrite (pf : cvc5.Proof) : ReconstructM (Option Expr) := do
   match pf.getRewriteRule with
