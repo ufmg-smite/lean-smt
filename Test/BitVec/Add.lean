04048378dabbb0c42862495f4e8e8574be57379e
Abdalrhman Mohamed <abdoo8080@outlook.com>
2024-05-08 17:36:04 -0700

    Support bitblasted addition. (#93)

--- Test/BitVec/Add.lean
+++ Test/BitVec/Add.lean
@@ -4,12 +4,12 @@ open BitVec
 
 example {x y : BitVec 4} : ¬(x = 11#4 ∧ y = 11#4 ∧ x + y = 15#4) := by
   smt
-  all_goals sorry
+  all_goals simp
 
 example {x y : BitVec 8} : ¬(x = 11#8 ∧ y = 11#8 ∧ x + y = 15#8) := by
   smt
-  all_goals sorry
+  all_goals simp
 
 example {x y : BitVec 16} : ¬(x = 11#16 ∧ y = 11#16 ∧ x + y = 15#16) := by
   smt
-  all_goals sorry
+  all_goals simp
