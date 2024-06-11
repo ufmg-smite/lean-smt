6144223547ec85993f6ac25bcfce12deb8ce6d90
Abdalrhman M Mohamed <abdoo8080@outlook.com>
2024-06-11 06:00:02 -0700

    Reprove theorems for proof rules and rewrites for integers.

---
+++ Smt/Reconstruct/Int/Lemmas.lean
@@ -0,0 +1,283 @@
+/-
+Copyright (c) 2021-2023 by the authors listed in the file AUTHORS and their
+institutional affiliations. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Tomaz Gomes Mascarenhas, Abdalrhman Mohamed
+-/
+
+private theorem Nat.mod_two_ne_zero : ¬n % 2 = 0 ↔ n % 2 = 1 := by
+  cases Nat.mod_two_eq_zero_or_one n <;> rename_i h <;> simp [h]
+
+private theorem Nat.even_add : (m + n) % 2 = 0 ↔ (m % 2 = 0 ↔ n % 2 = 0) := by
+  cases Nat.mod_two_eq_zero_or_one m <;> rename_i h₁ <;> cases Nat.mod_two_eq_zero_or_one n <;> rename_i h₂ <;>
+    simp [h₁, h₂, Nat.add_mod]
+
+private theorem Nat.even_add_one : (n + 1) % 2 = 0 ↔ n % 2 ≠ 0 := by
+  simp [Nat.even_add]
+
+private theorem Int.lt_of_sub_neg {a b : Int} (h : a - b < 0) : a < b := by
+  have h := Int.add_lt_add_right h b
+  rwa [Int.sub_add_cancel, Int.zero_add] at h
+
+private theorem Int.sub_neg_of_lt {a b : Int} (h : a < b) : a - b < 0 := by
+  have h := Int.add_lt_add_right h (-b)
+  rwa [Int.add_right_neg] at h
+
+private theorem Int.mul_lt_mul_of_neg_left {a b c : Int} (h : b < a) (hc : c < 0) : c * a < c * b := by
+  have : c * (a - b) < 0 := sorry
+  rw [Int.mul_sub] at this
+  exact Int.lt_of_sub_neg this
+
+private theorem Int.sub_nonpos_of_le {a b : Int} (h : a ≤ b) : a - b ≤ 0 := by
+  have h := Int.add_le_add_right h (-b)
+  rwa [Int.add_right_neg] at h
+
+private theorem Int.le_of_sub_nonpos {a b : Int} (h : a - b ≤ 0) : a ≤ b := by
+  have h := Int.add_le_add_right h b
+  rwa [Int.sub_add_cancel, Int.zero_add] at h
+
+private theorem Int.mul_lt_mul_left {c x y : Int} (hc : c > 0) : (c * x < c * y) = (x < y) := by
+  apply propext
+  constructor
+  · sorry
+  · apply Int.mul_lt_mul_of_pos_left (h₂ := hc)
+
+private theorem Int.mul_le_mul_left {c x y : Int} (hc : c > 0) : (c * x ≤ c * y) = (x ≤ y) := by
+  apply propext
+  constructor
+  · sorry
+  · apply Int.mul_le_mul_of_nonneg_left (h₂ := Int.le_of_lt hc)
+
+private theorem Int.mul_eq_zero_left {x y : Int} (hx : x ≠ 0) (hxy : x * y = 0) : y = 0 := by
+  rewrite [Int.mul_eq_zero] at hxy
+  exact hxy.resolve_left hx
+
+private def uncurry {p₁ p₂ p₃ : Prop} : (p₁ → p₂ → p₃) → (p₁ ∧ p₂) → p₃ := by
+  intros h₁ h₂
+  have ⟨ ht₁, ht₂ ⟩ := h₂
+  exact h₁ ht₁ ht₂
+
+namespace Smt.Reconstruct.Int
+
+variable {a b c d : Int}
+
+private theorem or_implies_left (hpq : p ∨ q) (hnq : ¬q) : p := by
+  cases hpq with
+  | inl hp => exact hp
+  | inr hq => exact False.elim (hnq hq)
+
+private theorem or_implies_right (hpq : p ∨ q) (hnp : ¬p) : q := by
+  cases hpq with
+  | inl hp => exact False.elim (hnp hp)
+  | inr hq => exact hq
+
+theorem sum_ub₁ (h₁ : a < b) (h₂ : c < d) : a + c < b + d := by
+  have r₁: a + c < a + d := Int.add_lt_add_left h₂ a
+  have r₂: a + d < b + d := Int.add_lt_add_right h₁ d
+  exact Int.lt_trans r₁ r₂
+
+theorem sum_ub₂ (h₁ : a < b) (h₂ : c ≤ d) : a + c < b + d := by
+  have r₁: a + c ≤ a + d := Int.add_le_add_left h₂ a
+  have r₂: a + d < b + d := Int.add_lt_add_right h₁ d
+  exact Int.lt_of_le_of_lt r₁ r₂
+
+theorem sum_ub₃ (h₁ : a < b) (h₂ : c = d) : a + c < b + d := by
+  rewrite [h₂]
+  exact Int.add_lt_add_right h₁ d
+
+theorem sum_ub₄ (h₁ : a ≤ b) (h₂ : c < d) : a + c < b + d := by
+  have r₁ : a + c < a + d := Int.add_lt_add_left h₂ a
+  have r₂ : a + d ≤ b + d := Int.add_le_add_right h₁ d
+  exact Int.lt_of_lt_of_le r₁ r₂
+
+theorem sum_ub₅ (h₁ : a ≤ b) (h₂ : c ≤ d) : a + c ≤ b + d := by
+  have r₁ : a + c ≤ a + d := Int.add_le_add_left h₂ a
+  have r₂ : a + d ≤ b + d := Int.add_le_add_right h₁ d
+  exact Int.le_trans r₁ r₂
+
+theorem sum_ub₆ (h₁ : a ≤ b) (h₂ : c = d) : a + c ≤ b + d := by
+  rewrite [h₂]
+  exact Int.add_le_add_right h₁ d
+
+theorem sum_ub₇ (h₁ : a = b) (h₂ : c < d) : a + c < b + d := by
+  rewrite [h₁]
+  exact Int.add_lt_add_left h₂ b
+
+theorem sum_ub₈ (h₁ : a = b) (h₂ : c ≤ d) : a + c ≤ b + d := by
+  rewrite [h₁]
+  exact Int.add_le_add_left h₂ b
+
+theorem sum_ub₉ (h₁ : a = b) (h₂ : c = d) : a + c ≤ b + d := by
+  rewrite [h₁, h₂]
+  exact Int.le_refl (b + d)
+
+theorem trichotomy₁ (h₁ : a ≥ b) (h₂ : a ≠ b) : a > b := by
+  have tr := Int.lt_trichotomy a b
+  exact or_implies_right (or_implies_right tr (Int.not_lt.mpr h₁)) h₂
+
+theorem trichotomy₂ (h₁ : a ≠ b) (h₂ : a ≥ b) : a > b := by
+  have tr := Int.lt_trichotomy a b
+  exact or_implies_right (or_implies_right tr (Int.not_lt.mpr h₂)) h₁
+
+theorem trichotomy₃ (h₁ : a ≥ b) (h₂ : a ≤ b) : a = b := by
+  have tr := Int.lt_trichotomy a b
+  exact or_implies_left (or_implies_right tr (Int.not_lt.mpr h₁)) (Int.not_lt.mpr h₂)
+
+theorem trichotomy₄ (h₁ : a ≤ b) (h₂ : a ≥ b) : a = b := by
+  have tr := Int.lt_trichotomy a b
+  exact or_implies_left (or_implies_right tr (Int.not_lt.mpr h₂)) (Int.not_lt.mpr h₁)
+
+theorem trichotomy₅ (h₁ : a ≠ b) (h₂ : a ≤ b) : a < b := by
+  have tr := Int.lt_trichotomy a b
+  exact or_implies_left (or_implies_left (or_assoc.mpr tr) (Int.not_lt.mpr h₂)) h₁
+
+theorem trichotomy₆ (h₁ : a ≤ b) (h₂ : a ≠ b) : a < b := by
+  have tr := Int.lt_trichotomy a b
+  exact or_implies_left (or_implies_left (or_assoc.mpr tr) (Int.not_lt.mpr h₁)) h₂
+
+theorem lt_eq_sub_lt_zero : (a < b) = (a - b < 0) := by
+  apply propext
+  constructor
+  · apply Int.sub_neg_of_lt
+  · apply Int.lt_of_sub_neg
+
+theorem le_eq_sub_le_zero : (a ≤ b) = (a - b ≤ 0) := by
+  apply propext
+  constructor
+  · exact Int.sub_nonpos_of_le
+  · exact Int.le_of_sub_nonpos
+
+theorem eq_eq_sub_eq_zero : (a = b) = (a - b = 0) := by
+  simp only [Int.sub_eq_zero]
+
+theorem ge_eq_sub_ge_zero : (a ≥ b) = (a - b ≥ 0) := by
+  simp only [ge_iff_le, eq_iff_iff]
+  constructor
+  · exact Int.sub_nonneg_of_le
+  · exact Int.le_of_sub_nonneg
+
+theorem gt_eq_sub_gt_zero : (a > b) = (a - b > 0) := by
+  simp only [gt_iff_lt, eq_iff_iff]
+  constructor
+  · exact Int.sub_pos_of_lt
+  · exact Int.lt_of_sub_pos
+
+theorem lt_of_sub_eq {c₁ c₂ : Int} (hc₁ : c₁ > 0) (hc₂ : c₂ > 0) (h : c₁ * (a₁ - a₂) = c₂ * (b₁ - b₂)) : (a₁ < a₂) = (b₁ < b₂) := by
+  have {c x y : Int} (hc : c > 0) : (c * (x - y) < 0) = (x - y < 0) := by
+    rw (config := { occs := .pos [1] }) [← Int.mul_zero c, Int.mul_lt_mul_left hc]
+  rw [lt_eq_sub_lt_zero, @lt_eq_sub_lt_zero b₁, ← this hc₁, ← this hc₂, h]
+
+theorem le_of_sub_eq {c₁ c₂ : Int} (hc₁ : c₁ > 0) (hc₂ : c₂ > 0) (h : c₁ * (a₁ - a₂) = c₂ * (b₁ - b₂)) : (a₁ ≤ a₂) = (b₁ ≤ b₂) := by
+  have {c x y : Int} (hc : c > 0) : (c * (x - y) ≤ 0) = (x - y ≤ 0) := by
+    rw (config := { occs := .pos [1] }) [← Int.mul_zero c, Int.mul_le_mul_left hc]
+  rw [le_eq_sub_le_zero, @le_eq_sub_le_zero b₁, ← this hc₁, ← this hc₂, h]
+
+theorem eq_of_sub_eq {c₁ c₂ : Int} (hc₁ : c₁ > 0) (hc₂ : c₂ > 0) (h : c₁ * (a₁ - a₂) = c₂ * (b₁ - b₂)) : (a₁ = a₂) = (b₁ = b₂) := by
+  have {c x y : Int} (hc : c > 0) : (c * (x - y) = 0) = (x - y = 0) := by
+    apply propext; constructor
+    · apply Int.mul_eq_zero_left (Int.ne_of_gt hc)
+    · intro hxy; rw [hxy, Int.mul_zero]
+  rw [@eq_eq_sub_eq_zero a₁, @eq_eq_sub_eq_zero b₁, ← this hc₁, ← this hc₂, h]
+
+theorem ge_of_sub_eq {c₁ c₂ : Int} (hc₁ : c₁ > 0) (hc₂ : c₂ > 0) (h : c₁ * (a₁ - a₂) = c₂ * (b₁ - b₂)) : (a₁ ≥ a₂) = (b₁ ≥ b₂) := by
+  have {c x y : Int} (hc : c > 0) : (c * (x - y) ≥ 0) = (x - y ≥ 0) := by
+    rw (config := { occs := .pos [1] }) [← Int.mul_zero c, ge_iff_le, Int.mul_le_mul_left hc]
+  rw [ge_eq_sub_ge_zero, @ge_eq_sub_ge_zero b₁, ← this hc₁, ← this hc₂, h]
+
+theorem gt_of_sub_eq {c₁ c₂ : Int} (hc₁ : c₁ > 0) (hc₂ : c₂ > 0) (h : c₁ * (a₁ - a₂) = c₂ * (b₁ - b₂)) : (a₁ > a₂) = (b₁ > b₂) := by
+  have {c x y : Int} (hc : c > 0) : (c * (x - y) > 0) = (x - y > 0) := by
+    rw (config := { occs := .pos [1] }) [← Int.mul_zero c, gt_iff_lt, Int.mul_lt_mul_left hc]
+  rw [gt_eq_sub_gt_zero, @gt_eq_sub_gt_zero b₁, ← this hc₁, ← this hc₂, h]
+
+mutual
+
+theorem pow_neg_even {k : Nat} {z : Int} (h₁ : z < 0) (h₂ : k % 2 = 0) : z ^ k > 0 := by
+  cases k with
+  | zero    => simp [Int.pow_zero]
+  | succ k =>
+    have hodd := Nat.even_add_one.mp h₂
+    have mulZ : z * z ^ k > z * 0 := Int.mul_lt_mul_of_neg_left (pow_neg_odd h₁ (Nat.mod_two_ne_zero.mp hodd)) h₁
+    simp at mulZ
+    rw [Int.pow_succ, Int.mul_comm]
+    exact mulZ
+
+theorem pow_neg_odd {k : Nat} {z : Int} (h₁ : z < 0) (h₂ : k % 2 = 1) : z ^ k < 0 := by
+  cases k with
+  | zero    => simp at h₂
+  | succ k =>
+    have heven : k % 2 = 0 := Classical.not_not.mp (mt Nat.even_add_one.mpr (Eq.symm h₂ ▸ nofun))
+    have mulZ : z * 0 > z * z ^ k := Int.mul_lt_mul_of_neg_left (pow_neg_even h₁ heven) h₁
+    simp at mulZ
+    rw [Int.pow_succ, Int.mul_comm]
+    exact mulZ
+
+end
+
+theorem pow_pos {k : Nat} {z : Int} (h : z > 0) : z ^ k > 0 := by
+  induction k with
+  | zero    => simp [Int.pow_zero]
+  | succ k ih =>
+    rw [Int.pow_succ]
+    have h₂ := Int.mul_lt_mul_of_pos_left h ih
+    simp at h₂
+    exact h₂
+
+theorem non_zero_even_pow {k : Nat} {z : Int} (h₁ : z ≠ 0) (h₂ : k % 2 = 0) : z ^ k > 0 := by
+  match Int.lt_trichotomy z 0 with
+  | Or.inl hneg => exact pow_neg_even hneg h₂
+  | Or.inr (Or.inl hzero) => rw [hzero] at h₁; simp at h₁
+  | Or.inr (Or.inr hpos)  => exact pow_pos hpos
+
+theorem combine_signs₁ : a > 0 → b > 0 → b * a > 0 := by
+  intros h₁ h₂
+  have h := Int.mul_lt_mul_of_pos_left h₁ h₂
+  simp at h
+  exact h
+
+theorem combine_signs₂ : a > 0 → b < 0 → b * a < 0 := by
+  intros h₁ h₂
+  have h := Int.mul_lt_mul_of_pos_right h₂ h₁
+  simp at h
+  exact h
+
+theorem combine_signs₃ : a < 0 → b > 0 → b * a < 0 := by
+  intros h₁ h₂
+  have h := Int.mul_lt_mul_of_pos_left h₁ h₂
+  simp at h
+  exact h
+
+theorem combine_signs₄ : a < 0 → b < 0 → b * a > 0 := by
+  intros h₁ h₂
+  have h := Int.mul_lt_mul_of_neg_left h₁ h₂
+  simp at h
+  exact h
+
+theorem arith_mul_pos_lt : c > 0 ∧ a < b → c * a < c * b :=
+  uncurry (flip Int.mul_lt_mul_of_pos_left)
+
+theorem arith_mul_pos_le : c > 0 ∧ a ≤ b → c * a ≤ c * b := λ h =>
+  Int.mul_le_mul_of_nonneg_left h.2 (Int.le_of_lt h.1)
+
+theorem arith_mul_pos_gt : c > 0 ∧ a > b → c * a > c * b := arith_mul_pos_lt
+
+theorem arith_mul_pos_ge : c > 0 ∧ a ≥ b → c * a ≥ c * b := arith_mul_pos_le
+
+theorem arith_mul_pos_eq : c > 0 ∧ a = b → c * a = c * b := by
+  intros h
+  rw [h.2]
+
+theorem arith_mul_neg_lt : c < 0 ∧ a < b → c * a > c * b :=
+  uncurry (flip Int.mul_lt_mul_of_neg_left)
+
+theorem arith_mul_neg_le : c < 0 ∧ a ≤ b → c * a ≥ c * b := λ h =>
+  Int.mul_le_mul_of_nonpos_left (Int.le_of_lt h.1) h.2
+
+theorem arith_mul_neg_gt : c < 0 ∧ a > b → c * a < c * b := arith_mul_neg_lt
+
+theorem arith_mul_neg_ge : c < 0 ∧ a ≥ b → c * a ≤ c * b := arith_mul_neg_le
+
+theorem arith_mul_neg_eq : c < 0 ∧ a = b → c * a = c * b := by
+  intros h
+  rw [h.2]
+
+end Smt.Reconstruct.Int
---
+++ Smt/Reconstruct/Int/Rewrites.lean
@@ -0,0 +1,105 @@
+/-
+Copyright (c) 2021-2023 by the authors listed in the file AUTHORS and their
+institutional affiliations. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Abdalrhman Mohamed
+-/
+
+namespace Smt.Reconstruct.Int.Rewrite
+
+open Function
+
+-- https://github.com/cvc5/cvc5/blob/proof-new/src/theory/arith/rewrites
+
+variable {t ts x xs : Int}
+
+theorem plus_zero : ts + 0 + ss = ts + ss :=
+  (Int.add_zero ts).symm ▸ rfl
+
+theorem mul_one : ts * 1 * ss = ts * ss :=
+  (_root_.Int.mul_one ts).symm ▸ rfl
+theorem mul_zero : ts * 0 * ss = 0 :=
+  (_root_.Int.mul_zero ts).symm ▸ (Int.zero_mul ss).symm ▸ rfl
+
+theorem int_div_total {t s : Int} : s ≠ 0 → t / s = t / s :=
+  const _ rfl
+theorem int_div_total_one {t : Int} : t / 1 = t :=
+  Int.ediv_one t
+theorem int_div_total_zero {t : Int} : t / 0 = 0 :=
+  Int.ediv_zero t
+
+theorem int_mod_total {t s : Int} : s ≠ 0 → t % s = t % s :=
+  const _ rfl
+theorem int_mod_total_one {t : Int} : t % 1 = 0 :=
+  Int.emod_one t
+theorem int_mod_total_zero {t : Int} : t % 0 = t :=
+  Int.emod_zero t
+
+theorem neg_neg_one : -1 * (-1 * t) = t :=
+  Int.neg_mul _ t ▸ (Int.one_mul t).symm ▸ Int.neg_mul_neg _ t ▸ (Int.one_mul t).symm ▸ rfl
+
+-- Eliminations
+
+theorem elim_uminus : -t = -1 * t :=
+  Int.neg_eq_neg_one_mul t ▸ rfl
+theorem elim_minus : t - s = t + -1 * s :=
+  Int.neg_eq_neg_one_mul s ▸ Int.sub_eq_add_neg ▸ rfl
+theorem elim_gt : (t > s) = ¬(t ≤ s) :=
+  propext Int.not_le.symm
+theorem elim_lt : (t < s) = ¬(t ≥ s) :=
+  propext Int.not_le.symm
+theorem elim_int_gt {t s : Int} : (t > s) = (t ≥ s + 1) :=
+  propext (Int.lt_iff_add_one_le s t)
+theorem elim_int_lt {t s : Int} : (t < s) = (s ≥ t + 1) :=
+  propext (Int.lt_iff_add_one_le t s)
+theorem elim_leq : (t ≤ s) = (s ≥ t) :=
+  propext ge_iff_le
+
+theorem leq_norm {t s : Int} : (t ≤ s) = ¬(t ≥ s + 1) :=
+  propext ⟨fun hts => Int.not_le.mpr (Int.add_le_add_right hts _),
+           Int.not_lt.mp⟩
+
+theorem geq_tighten {t s : Int} : (¬(t ≥ s)) = (s ≥ t + 1) :=
+  propext Int.not_le
+
+theorem geq_norm1 : (t ≥ s) = (t - s ≥ 0) :=
+  propext ⟨Int.sub_nonneg_of_le, Int.le_of_sub_nonneg⟩
+
+theorem geq_norm2 : (t ≥ s) = (-t ≤ -s) :=
+  propext ⟨Int.neg_le_neg, Int.le_of_neg_le_neg⟩
+
+theorem refl_leq : (t ≤ t) = True :=
+  propext ⟨const _ trivial, const _ (Int.le_refl t)⟩
+theorem refl_lt : (t < t) = False :=
+  propext ⟨(Int.lt_irrefl t), False.elim⟩
+theorem refl_geq : (t ≥ t) = True :=
+  propext ⟨const _ trivial, const _ (Int.le_refl t)⟩
+theorem refl_gt : (t > t) = False :=
+  propext ⟨(Int.lt_irrefl t), False.elim⟩
+
+theorem int_eq_elim {t s : Int} : (t = s) = (t ≥ s ∧ t ≤ s) :=
+  propext ⟨(· ▸ And.intro (Int.le_refl t) (Int.le_refl t)), fun ⟨hst, hts⟩ => Int.le_antisymm hts hst⟩
+
+theorem plus_flatten : xs + (w + ys) + zs = xs + w + ys + zs :=
+  Int.add_assoc xs w ys ▸ rfl
+
+theorem mult_flatten : xs * (w * ys) * zs = xs * w * ys * zs :=
+  Int.mul_assoc xs w ys ▸ rfl
+
+theorem mult_dist : x * (y + z + ws) = x * y + x * (z + ws) :=
+  Int.add_assoc y z ws ▸ Int.mul_add x y (z + ws) ▸ rfl
+
+theorem plus_cancel1 : ts + x + ss + (-1 * x) + rs = ts + ss + rs :=
+  Int.neg_eq_neg_one_mul x ▸ Int.add_assoc ts x ss ▸ Int.add_assoc ts (x + ss) (-x) ▸
+  Int.add_comm x ss ▸ (Int.add_neg_cancel_right ss x).symm ▸ rfl
+theorem plus_cancel2 : ts + (-1 * x) + ss + x + rs = ts + ss + rs :=
+  Int.neg_eq_neg_one_mul x ▸ Int.add_assoc ts (-x) ss ▸ Int.add_assoc ts (-x + ss) x ▸
+  Int.add_comm (-x) ss ▸ (Int.neg_add_cancel_right ss x).symm ▸ rfl
+
+-- theorem abs_elim : |x| = if x < 0 then -x else x :=
+--   if h : x < 0 then
+--     if_pos h ▸ abs_of_neg h
+--   else
+--     if_neg h ▸ abs_of_nonneg (le_of_not_lt h)
+
+end Smt.Reconstruct.Int.Rewrite
