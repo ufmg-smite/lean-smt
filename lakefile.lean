bef4fe37ad5e8be8b18423d81d66cc92e17557cd
Adrien Champion <adrien.champion@protonmail.com>
2024-06-11 17:01:33 +0200

    chore: cvc5 dependency

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,7 @@ import Lake
 open Lake DSL
 
 require cvc5 from
-  git "https://github.com/abdoo8080/lean-cvc5.git"
+  git "https://github.com/abdoo8080/lean-cvc5.git" @ "main"
 
 require mathlib from
   git "https://github.com/leanprover-community/mathlib4.git" @ "v4.8.0"
