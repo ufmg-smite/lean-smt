5b92d622cccca47b86747a2bc1cc5edfcbb3a8b7
Harun Khan <58151072+mhk119@users.noreply.github.com>
2024-08-12 16:42:52 -0700

    Update Smt/Reconstruct/Int/Polynorm.lean
    Co-authored-by: Abdalrhman Mohamed <abdoo8080@outlook.com>

--- Smt/Reconstruct/Int/Polynorm.lean
+++ Smt/Reconstruct/Int/Polynorm.lean
@@ -97,7 +97,7 @@ theorem foldl_mul_insert {ctx : Context} :
   List.foldl (fun z a => z * (ctx a)) 1 (mul.insert y ys) =
   (ctx y) * List.foldl (fun z a => z * (ctx a)) 1 ys := by
   induction ys with
-  | nil => simp [mul.insert]
+  | nil => rfl
   | cons x ys ih =>
     by_cases h : y ≤ x
     · simp [mul.insert, h, foldl_assoc Int.mul_assoc (ctx y) (ctx x), Int.mul_assoc]
