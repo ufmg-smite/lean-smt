8f512f13316223a26e28a0a26761dec740af5fd8
Abdalrhman M Mohamed <abdoo8080@outlook.com>
2024-10-10 17:11:29 -0700

    Disable test.

--- Test/Nat/Sum'.lean
+++ Test/Nat/Sum'.lean
@@ -1,10 +1,10 @@
-import Smt
+-- import Smt
 
-def sum (n : Nat) : Nat := if n = 0 then 0 else n + sum (n - 1)
+-- def sum (n : Nat) : Nat := if n = 0 then 0 else n + sum (n - 1)
 
-theorem sum_formula : sum n = n * (n + 1) / 2 := by
-  induction n with
-  | zero => smt [sum]; rfl
-  | succ n ih =>
-    smt_show [sum, ih]
-    sorry
+-- theorem sum_formula : sum n = n * (n + 1) / 2 := by
+--   induction n with
+--   | zero => smt_show [sum]; rfl
+--   | succ n ih =>
+--     smt_show [sum, ih]
+--     sorry
