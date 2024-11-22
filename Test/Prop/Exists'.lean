a6271abe26f92900b26f15a62e77e43fb76bd676
Abdalrhman Mohamed <abdoo8080@outlook.com>
2024-11-22 01:40:25 -0800

    Support more quantifier rewrites. (#147)
    * Support more quantifier rewrites.
    
    * Cleanup proofs.

--- Test/Prop/Exists'.lean
+++ Test/Prop/Exists'.lean
@@ -2,8 +2,3 @@ import Smt
 
 theorem exists' : ∃ p : Prop, p := by
   smt
-  · intro; apply propext; apply Iff.intro
-    · intro hp ht
-      cases hp True <;> contradiction
-    · intro hnt
-      contradiction
