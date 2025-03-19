3270582e2b701fa9536908c78acb6c745b985497
Tomaz <tomgm1502@gmail.com>
2025-03-19 11:52:03 -0300

    Add module file

---
+++ Smt/Reconstruct/Arith/MultAbsComparison.lean
@@ -0,0 +1,11 @@
+/-
+Copyright (c) 2021-2025 by the authors listed in the file AUTHORS and their
+institutional affiliations. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Tomaz Gomes Mascarenhas
+-/
+
+-- implementation of:
+-- https://cvc5.github.io/docs/cvc5-1.2.1/api/cpp/enums/proofrule.html#_CPPv4N4cvc59ProofRule25ARITH_MULT_ABS_COMPARISONE
+
+import Smt.Reconstruct.Arith.MultAbsComparison.Lemmas
