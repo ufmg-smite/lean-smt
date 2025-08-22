4799ae452be5ebfb4e77118cfb74798c8480bfad
Abdalrhman M Mohamed <abdoo8080@outlook.com>
2025-08-21 17:44:52 -0700

    Turn auto into an optional preprocessing step inside smt.

--- Test/Auto.lean
+++ Test/Auto.lean
@@ -2,31 +2,26 @@ import Mathlib.Algebra.Group.Defs
 import Mathlib.Data.Set.Function
 
 import Smt
-import Smt.Auto
-
-set_option auto.native true
-
-attribute [rebind Auto.Native.solverFunc] Smt.smtSolverFunc
 
 example (x : α) : List.head? [x] = .some x := by
   have h₂ : ∀ (x : α) (ys : _), List.head? (x :: ys) = .some x := fun _ _ => rfl
-  auto
+  smt +mono
 
 example (x y : α) : [x] ++ [y] = [x, y] := by
   -- Invoke definition unfolding
   have h : ∀ (x y : List α), x ++ y = x.append y := fun _ _ => rfl
-  auto [h] d[List.append]
+  smt +mono [h]
 
 variable [Group G]
 
 theorem inverse' : ∀ (a : G), a * a⁻¹ = 1 := by
-  auto [mul_assoc, one_mul, inv_mul_cancel]
+  smt +mono [mul_assoc, one_mul, inv_mul_cancel]
 
 theorem identity' : ∀ (a : G), a * 1 = a := by
-  auto [mul_assoc, one_mul, inv_mul_cancel, inverse']
+  smt +mono [mul_assoc, one_mul, inv_mul_cancel, inverse']
 
 theorem unique_identity (e : G) : (∀ z, e * z = z) → e = 1 := by
-  auto [mul_assoc, one_mul, inv_mul_cancel]
+  smt +mono [mul_assoc, one_mul, inv_mul_cancel]
 
 -- TODO: pre-process Nat away
 -- example (x y : α) : List.get? [x, y] 1 = .some y := by
@@ -39,14 +34,14 @@ set_option auto.mono.termLikeDefEq.mode "reducible"
 variable [Nonempty α] [Nonempty β] {f : α → β} {s : Set α} {v u : Set β}
 
 example : f '' s ⊆ v ↔ s ⊆ f ⁻¹' v := by
-  auto [Set.subset_def, Set.mem_image, Set.mem_preimage]
+  smt +mono [Set.subset_def, Set.mem_image, Set.mem_preimage]
 
 example (h : Function.Injective f) : f ⁻¹' (f '' s) ⊆ s := by
-  auto [Set.subset_def, Set.mem_preimage, Function.Injective.mem_set_image, h]
+  smt +mono [Set.subset_def, Set.mem_preimage, Function.Injective.mem_set_image, h]
 
 example : f '' (f ⁻¹' u) ⊆ u := by
-  auto [Set.subset_def, Set.mem_image, Set.mem_preimage]
+  smt +mono [Set.subset_def, Set.mem_image, Set.mem_preimage]
 
 example (h : Function.Surjective f) : u ⊆ f '' (f ⁻¹' u) := by
   unfold Function.Surjective at h
-  auto [Set.subset_def, Set.mem_image, Set.mem_preimage, h]
+  smt +mono [Set.subset_def, Set.mem_image, Set.mem_preimage, h]
