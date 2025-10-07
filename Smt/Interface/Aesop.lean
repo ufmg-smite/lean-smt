b8aa4ab70a2d6e62df7007cab4f106d8102b31e0
Tomaz Gomes Mascarenhas <tomgm1502@gmail.com>
2025-10-06 23:37:36 -0300

    Fix bug on aesop interface

--- Smt/Interface/Aesop.lean
+++ Smt/Interface/Aesop.lean
@@ -12,47 +12,61 @@ import Qq
 
 open Lean Meta Parser Elab Tactic Syntax Aesop Qq
 
--- The actual expr of the premisses and the corresponding syntax
+-- The type and the corresponding syntax
 abbrev Premises := List (Expr × Syntax)
 
 def cast_stx (stx : Syntax) : BaseM (TSyntax `term) :=
   match stx with
   | `($t) => return t
 
+def cast_stx' (stx : Syntax) : TacticM (TSyntax `term) :=
+  match stx with
+  | `($t) => return t
+
+def createArrow (es : List Expr) (e : Expr) : Expr :=
+  match es with
+  | [] => e
+  | hd :: tl =>
+    let r : Q(Prop) := createArrow tl e
+    let hd : Q(Prop) := hd
+    q($hd -> $r)
+
 def smtSingleRuleTac (ps : Premises) (includeLCtx : Bool) : SingleRuleTac := fun input => do
   let preState ← saveState
-  --  input.goal.withContext
-  -- NOTE: I am removing this for now as it seems to remove visibility from the main goal hypothesis. The tactic
-  -- seems to still work without it.
-  let goal_copy := (← Meta.mkFreshExprMVar (← input.goal.getType)).mvarId!
-  let res ← Smt.smt default goal_copy (ps.map (fun p => p.1)).toArray
-  let unsat_core ←
-    match res with
-    | .unsat mvs uc => pure uc
-    | _ => throwError "[Smt.smt]: Got SAT from solver"
-  let names := unsat_core.map (fun e => (ps.lookup e).get!)
-  let namesT ← names.mapM cast_stx
-  let idents ← namesT.mapM (fun i => `(Smt.Tactic.smtHintElem| $i:term))
-  let stx ←
-    if includeLCtx && !unsat_core.isEmpty then
-      `(tactic| smt [*, $(idents),*])
-    else if includeLCtx && unsat_core.isEmpty then
-      `(tactic| smt [*])
-    else if !includeLCtx && !unsat_core.isEmpty then
-      `(tactic| smt [$(idents),*])
-    else -- if !includeLCtx && unsat_core.isEmpty
-      `(tactic| smt)
-  let tac := withoutRecover $ evalTactic stx
-  let postGoals := (← Elab.Tactic.run input.goal tac |>.run').toArray
-  let postState ← saveState
-  let tacticBuilder : MetaM Script.Tactic := pure $ .unstructured ⟨stx⟩
-  let step : Script.LazyStep := {
-    preGoal := input.goal
-    tacticBuilders := #[tacticBuilder]
-    preState, postState, postGoals
-  }
-  let postGoals ← postGoals.mapM (mvarIdToSubgoal input.goal ·)
-  return (postGoals, some #[step], some ⟨1.0⟩)
+  input.goal.withContext do
+    let g ← input.goal.getType
+    let types := ps.map (fun p => p.1)
+    let arrow := createArrow types g
+    let mv ← mkFreshExprMVar arrow
+    let (hs, mv') ← mv.mvarId!.introN ps.length
+    let res ← Smt.smt default mv' (hs.map (fun fv => .fvar fv))
+    let unsat_core ←
+      match res with
+      | .unsat mvs uc => pure uc
+      | _ => throwError "[Smt.smt]: Got SAT from solver"
+    let names := unsat_core.map (fun e => let idx := hs.findIdx (fun z => .fvar z == e); ps[idx]!.2)
+    let namesT ← names.mapM cast_stx
+    let idents ← namesT.mapM (fun i => `(Smt.Tactic.smtHintElem| $i:term))
+    let stx ←
+      if includeLCtx && !unsat_core.isEmpty then
+        `(tactic| smt [*, $(idents),*])
+      else if includeLCtx && unsat_core.isEmpty then
+        `(tactic| smt [*])
+      else if !includeLCtx && !unsat_core.isEmpty then
+        `(tactic| smt [$(idents),*])
+      else -- if !includeLCtx && unsat_core.isEmpty
+        `(tactic| smt)
+    let tac := withoutRecover $ evalTactic stx
+    let postGoals := (← Elab.Tactic.run input.goal tac |>.run').toArray
+    let postState ← saveState
+    let tacticBuilder : MetaM Script.Tactic := pure $ .unstructured ⟨stx⟩
+    let step : Script.LazyStep := {
+      preGoal := input.goal
+      tacticBuilders := #[tacticBuilder]
+      preState, postState, postGoals
+    }
+    let postGoals ← postGoals.mapM (mvarIdToSubgoal input.goal ·)
+    return (postGoals, some #[step], some ⟨1.0⟩)
 
 -- Example using the above function to integrate lean-smt into aesop
 
@@ -61,14 +75,17 @@ syntax (name := foo) "foo" ("[" term,* "]")? : tactic
 def parseFoo : Syntax → TacticM (List (Expr × Syntax))
   | `(tactic| foo) => pure []
   | `(tactic| foo [ $[$ns],* ]) => do
-      let exprs ← ns.toList.mapM (fun t => elabTerm t.raw none)
-      return List.zip exprs ns.toList
+      let hints ← ns.mapM (fun n => cast_stx' n)
+      let hints ← hints.mapM (fun n => `(Smt.Tactic.smtHintElem| $n:term))
+      let (_, elabedHints) ← Smt.Tactic.elabHints (← `(Smt.Tactic.smtHints| [$(hints),*]))
+      let types ← elabedHints.mapM (fun h => Meta.inferType h)
+      return List.zip types.toList ns.toList
   | _ => throwError "[foo]: unexpected syntax"
 
 @[tactic foo]
 def evalFoo : Tactic := fun stx => withMainContext do
-  let names : List (Expr × Syntax) ← parseFoo stx
-  let ruleTacVal ← mkAppM `smtSingleRuleTac #[q($names), q(false)]
+  let typesAndStx : List (Expr × Syntax) ← parseFoo stx
+  let ruleTacVal ← mkAppM `smtSingleRuleTac #[q($typesAndStx), q(false)]
   let ruleTacType := mkConst `Aesop.SingleRuleTac
   let ruleTacDecl :=
     mkDefinitionValEx `instantiatedSmtCoreRuleTac [] ruleTacType ruleTacVal ReducibilityHints.opaque DefinitionSafety.safe [`instantiatedSmtCoreRuleTac]
