994789d4414f39f50a66e5e6843744178536e680
Tomaz Gomes Mascarenhas <tomgm1502@gmail.com>
2026-03-19 15:09:04 -0300

    add example

--- A.lean
+++ A.lean
@@ -5,6 +5,4 @@ set_option trace.smt true
 set_option trace.smt.reconstruct.proof true
 set_option trace.smt.solve true
 
-
-
 example (a : Real) : a > 3 / 2 → a * a + a / 5 = 3 / 2 → False := by smt
