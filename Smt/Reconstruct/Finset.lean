fe64ccc2a31dae8d28731ca5f2d2e8300a1e63f7
Abdalrhman M Mohamed <abdoo8080@outlook.com>
2026-03-27 01:54:43 -0700

    Finish term reconstruction and reconstruct more proofs.

--- Smt/Reconstruct/Finset.lean
+++ Smt/Reconstruct/Finset.lean
@@ -18,25 +18,23 @@ namespace Smt.Reconstruct.ZMod
 open Lean Qq
 
 
-@[smt_sort_reconstruct] def reconstructSetSort : SortReconstructor := fun s => do match s.getKind with
-  | .SET_SORT=>
-    let u ← Meta.getLevel (← reconstructSort s.getSetElementSort!)
-    let α : Q(Type u) ← reconstructSort s.getSetElementSort!
-    return q(Finset $α)
-  | _             => return none
+-- @[smt_sort_reconstruct] def reconstructSetSort : SortReconstructor := fun s => do match s.getKind with
+--   | .SET_SORT=>
+--     let u ← Meta.getLevel (← reconstructSort s.getSetElementSort!)
+--     let α : Q(Type u) ← reconstructSort s.getSetElementSort!
+--     return q(Finset $α)
+--   | _             => return none
 
-#check Finset.instIsEmpty
-
-  @[smt_term_reconstruct] def reconstructSetOp : TermReconstructor := fun t => do match t.getKind with
-  | .SET_IS_EMPTY =>
-    let u ← Meta.getLevel (← reconstructSort t.getChildren[0]!.getSort)
-    let α : Q(Type u) ← reconstructSort t.getChildren[0]!.getSort
-    let s : Q(Finset $α) ← reconstructTerm t.getChildren[0]!
-    return q($s = ∅)
-  | _ => return none
-where
-  leftAssocOp (op : Expr) (t : cvc5.Term) : ReconstructM Expr := do
-    let mut curr ← reconstructTerm t[0]!
-    for i in [1:t.getNumChildren] do
-      curr := mkApp2 op curr (← reconstructTerm t[i]!)
-    return curr
+--   @[smt_term_reconstruct] def reconstructSetOp : TermReconstructor := fun t => do match t.getKind with
+--   | .SET_IS_EMPTY =>
+--     let u ← Meta.getLevel (← reconstructSort t.getChildren[0]!.getSort)
+--     let α : Q(Type u) ← reconstructSort t.getChildren[0]!.getSort
+--     let s : Q(Finset $α) ← reconstructTerm t.getChildren[0]!
+--     return q($s = ∅)
+--   | _ => return none
+-- where
+--   leftAssocOp (op : Expr) (t : cvc5.Term) : ReconstructM Expr := do
+--     let mut curr ← reconstructTerm t[0]!
+--     for i in [1:t.getNumChildren] do
+--       curr := mkApp2 op curr (← reconstructTerm t[i]!)
+--     return curr
