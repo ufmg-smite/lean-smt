6a887febb321bd5006b526f6c482ba327de3dd83
Tomaz Gomes Mascarenhas <tomgm1502@gmail.com>
2026-05-09 22:22:40 -0300

    modify tests file

---
+++ NL_UNIV_TESTS.lean
@@ -0,0 +1,31 @@
+import Smt
+import Smt.Real
+
+-- v1: 3372ms
+-- v2: 3009ms
+lemma exemplo (a : Real) : ¬ -1 * a ≥ -3 / 2 → a = 15 / 2 + -5 * (a * a) → False := by
+  smt
+
+#print axioms exemplo
+
+-- v1: 3810ms
+-- v2: 3287ms
+lemma l1 (a : Real) : a * a = 3 → a * a * a + 11 < 2 → False := by smt
+
+#print axioms l1
+
+-- v1 (7211561): 2244ms
+-- v2 (b81b158): 2275ms
+lemma ex1 (x : Real) : (¬ (x ≥ -9 ∧ x < 10 ∧ x * x * x * x > 0) ∨ (x * x * x * x * x * x * x * x * x * x * x * x > 0)) := by
+  smt
+
+#print axioms ex1
+
+-- v1: 9079ms
+-- v2: 9051ms
+lemma ex2 (x : Real) :
+    ¬ ((x - 2) * (x - 2) * (-x + 4) > 0 ∧ x * x * (x - 3) * (x - 3) ≥ 0 ∧ x - 1 ≥ 0 ∧ -(x - 2) * (x - 2) + 1 > 0)
+    ∨ (-(x - 11/12)) * (-(x - 11/12)) * (-(x - 11/12)) * (x - 41/10) * (x - 41/10) * (x - 41/10) ≥ 0 := by
+  smt
+
+#print axioms ex2
