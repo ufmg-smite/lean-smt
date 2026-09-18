18c6251a5a0f104812ad60b75436ac771a3940e4
Tomaz Gomes Mascarenhas <tomgm1502@gmail.com>
2026-09-18 14:54:44 -0300

    Update tests file

--- A.lean
+++
@@ -1,7 +0,0 @@
-import Smt
-import Smt.Real
-set_option trace.smt true in
-set_option trace.smt.reconstruct true in
-set_option trace.smt.reconstruct.proof true in
-example (a : Real) : a > 3 / 2 → a * a + a / 5 = 3 / 2 → False := by
-  smt
--- NL_UNIV_TESTS.lean
+++ NL_UNIV_TESTS.lean
@@ -1,6 +1,8 @@
 import Smt
 import Smt.Real
 
+set_option trace.smt.reconstruct.proof true
+
 -- v1: 3372ms
 -- v2: 3009ms
 -- v3: 1007ms
@@ -9,28 +11,28 @@ lemma exemplo (a : Real) : ¬ -1 * a ≥ -3 / 2 → a = 15 / 2 + -5 * (a * a) �
 
 #print axioms exemplo
 
--- v1: 3810ms
--- v2: 3287ms
--- v3: 1084ms
-lemma l1 (a : Real) : a * a = 3 → a * a * a + 11 < 2 → False := by
-  smt
+/- -- v1: 3810ms -/
+/- -- v2: 3287ms -/
+/- -- v3: 1084ms -/
+/- lemma l1 (a : Real) : a * a = 3 → a * a * a + 11 < 2 → False := by -/
+/-   smt -/
 
-#print axioms l1
+/- #print axioms l1 -/
 
--- v1 (7211561): 2244ms
--- v2 (b81b158): 2275ms
--- v3 (bfea4e7): 1093ms
-lemma ex1 (x : Real) : (¬ (x ≥ -9 ∧ x < 10 ∧ x * x * x * x > 0) ∨ (x * x * x * x * x * x * x * x * x * x * x * x > 0)) := by
-  smt
+/- -- v1 (7211561): 2244ms -/
+/- -- v2 (b81b158): 2275ms -/
+/- -- v3 (bfea4e7): 1093ms -/
+/- lemma ex1 (x : Real) : (¬ (x ≥ -9 ∧ x < 10 ∧ x * x * x * x > 0) ∨ (x * x * x * x * x * x * x * x * x * x * x * x > 0)) := by -/
+/-   smt -/
 
-#print axioms ex1
+/- #print axioms ex1 -/
 
--- v1: 9079ms
--- v2: 9051ms
--- v3: 2936ms
-lemma ex2 (x : Real) :
-    ¬ ((x - 2) * (x - 2) * (-x + 4) > 0 ∧ x * x * (x - 3) * (x - 3) ≥ 0 ∧ x - 1 ≥ 0 ∧ -(x - 2) * (x - 2) + 1 > 0)
-    ∨ (-(x - 11/12)) * (-(x - 11/12)) * (-(x - 11/12)) * (x - 41/10) * (x - 41/10) * (x - 41/10) ≥ 0 := by
-  smt +native
+/- -- v1: 9079ms -/
+/- -- v2: 9051ms -/
+/- -- v3: 2936ms -/
+/- lemma ex2 (x : Real) : -/
+/-     ¬ ((x - 2) * (x - 2) * (-x + 4) > 0 ∧ x * x * (x - 3) * (x - 3) ≥ 0 ∧ x - 1 ≥ 0 ∧ -(x - 2) * (x - 2) + 1 > 0) -/
+/-     ∨ (-(x - 11/12)) * (-(x - 11/12)) * (-(x - 11/12)) * (x - 41/10) * (x - 41/10) * (x - 41/10) ≥ 0 := by -/
+/-   smt +native -/
 
-#print axioms ex2
+/- #print axioms ex2 -/
